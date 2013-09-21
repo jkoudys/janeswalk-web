249f1a932c0690b5dd1ac9fb64ad1710fdff777c
Joshua Koudys <Joshua Koudys>
2013-09-21 23:05:54 +0000

    Link dropdown to cities in DB

--- themes/janeswalk/home.php
+++ themes/janeswalk/home.php
@@ -1,5 +1,6 @@
 <?php 
 defined('C5_EXECUTE') or die(_("Access Denied."));
+$nh = Loader::helper('navigation');
 ?>
 <!DOCTYPE html>
 <?php $this->inc('elements/header.php');  ?>
@@ -25,8 +26,15 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
     <div class="navbar_home">
       <form class="input-append" action="http://janeswalk.tv/toronto.html" method="get" autocomplete="off">
         <select class="span3 large" name="URL" onchange="window.location.href=this.form.URL.options[this.form.URL.selectedIndex].value">
-          <option value="/toronto.html" selected="selected">Select a City</option>
-          <option value="/toronto.html">Toronto</option>
+          <option selected="selected">Select a City</option>
+          <?php
+          Loader::model('page_list'); 
+          $cities = new PageList();
+          $cities->filterByCollectionTypeHandle('city');
+          foreach($cities->get() as $city) {
+          ?>
+            <option value="<?php echo $nh->getCollectionURL($city)?>"><?php echo $city->getCollectionName() ?></option>
+          <? } ?>
         </select> 
       </form>
     </div>
