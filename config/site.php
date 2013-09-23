38e5539d5be0c0ada4dfb75cd3da16a790986aa6
Joshua Koudys <Joshua Koudys>
2013-09-23 07:57:38 +0000

    Take db config out of repo

--- config/site.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'jwalknet_c5');
-define('DB_PASSWORD', '8ROch9eR5HIKsYGVv');
-define('DB_DATABASE', 'yearround');
-define('PASSWORD_SALT', 'z7NoEKmcyikiFqsU8b1pwnRvHu89VB70nr4XHnT6EpPlKNzo77HS9CLUlIArepPN');
-define('PAGE_TITLE_FORMAT', '%2$s | %1$s');
-
