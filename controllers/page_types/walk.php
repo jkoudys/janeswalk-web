b92fb26f781644db823e4f0fe62709d779d57339
Joshua Koudys <Joshua Koudys>
2013-10-08 23:57:17 -0400

    twk

--- controllers/page_types/walk.php
+++ controllers/page_types/walk.php
@@ -77,12 +77,12 @@
         try{
           $response = $eb_client->event_new($new_event_params);
           $c->setAttribute("eventbrite", $response->id);
-          var_dump($response);
         }catch( Exception $e ){
           // application-specific error handling goes here
           $response = $e->error;
         }
       }
+      var_dump($response);
 
         echo json_encode($walkData);
     }
