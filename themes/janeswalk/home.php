6a67d17a5050b5d5c286aaab5c9395b4066c0b7e
Joshua Koudys <Joshua Koudys>
2013-12-06 18:15:33 -0500

    Set backup fontset for museo-slab

--- themes/janeswalk/home.php
+++ themes/janeswalk/home.php
@@ -12,7 +12,7 @@ global $u; global $cp;
   <div class="intro full">
 
   <div class="callouts">
-    <blockquote class="homepage-callout2 tk-museo-slab">
+    <blockquote class="homepage-callout2">
       <?php $a = new Area('Intro'); $a->display($c); ?>
     </blockquote> 
 
@@ -43,7 +43,7 @@ global $u; global $cp;
   
 <div class="row-fluid row1">
 
-  <ul class="nav nav-tabs types tk-museo-slab box-sizing">
+  <ul class="nav nav-tabs types box-sizing">
     <li class=""><a href="#festival" data-toggle="tab">Festival Weekend</a></li>
     <li class="active"><a href="#YearRoundWalks" data-toggle="tab">YearRound Walks</a></li>
     <li class=""><a href="#place-based-storytelling" data-toggle="tab">Walk Stories</a></li>
@@ -55,7 +55,7 @@ global $u; global $cp;
       <div class="festivalblock  clearfix">
         <div class="row-fluid">
           <div class="festivalcontent span6 pull-left">
-          <h3 class="tk-museo-slab">Festival Weekend: <br>May 4th and 5th!</h3>
+          <h3 class="">Festival Weekend: <br>May 4th and 5th!</h3>
           <p>Every year we hold a festival where people from all walks of life, in every corner of the world, offer free and open tours of where they live, inviting friends and the wider community to join them in a discussion of what works and what doesn't about the city around them. The walks celebrate the life and legacy of Jane Jacobs, a woman whose writing changed the way our cities are planned by making a case for all of us to "get out and walk"-- seeing lofty city plans from the perspective of our sidewalks. Today, the walks happen in over 100 cities, from Calgary to Calcutta, Sydney to Saskatoon.</p>
         </div>
         <div class="festivalimg span6 pull-right">
@@ -70,7 +70,7 @@ global $u; global $cp;
       <div class="festivalblock clearfix">
         <div class="row-fluid">
           <div class="festivalcontent span6 pull-left">
-          <h3 class="tk-museo-slab">YearRound Walks</h3>
+          <h3>YearRound Walks</h3>
           <p>Jane’s Walk can now be enjoyed outside of the festival weekend in May at anytime throughout the year.  If you savour the Jane’s Walk format of a walking conversation, community voices, and the opportunity to learn about new and often overlooked places within a city, the Jane’s Walk YearRound program is for you.  The online booking system can be used to register for select Jane’s Walks or request walks of specific subject or place within the city.  Enjoy the special experience of Jane’s Walk with visitors, your conference, group, or simply your own desire to learn more about your city.</p>
         </div>
         <div class="festivalimg span6 pull-right">
@@ -84,7 +84,7 @@ global $u; global $cp;
       <div class="festivalblock clearfix">
         <div class="row-fluid">
           <div class="festivalcontent span6 pull-left">
-          <h3 class="tk-museo-slab">Walk Stories</h3>
+          <h3>Walk Stories</h3>
           <p>Do you have a walk that you would like to share with friends, family or would like to organize a walk for others? The Jane’s Walk – “Walk Stories” online tool allows you to easily create and share a walk story.  Build your own map with places of interest, a route through a neighbourhood or area of the city, and stories about these places.  Encourage others to “get out and walk” to enjoy and learn about the city.  Share through a web browser on your desktop or smartphone or publish for others to view.</p>
         </div>
         <div class="festivalimg span6 pull-right">
