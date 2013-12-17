8f06661004ed4077fa41ed29e44c05d76b6e5df6
Joshua Koudys <Joshua Koudys>
2013-12-17 01:12:27 -0500

    Cleanup code

--- themes/janeswalk/blog_entry.php
+++ themes/janeswalk/blog_entry.php
@@ -5,22 +5,18 @@ $this->inc('elements/header.php');
 $headImage = $c->getAttribute("main_image");
 ?>
 	
-<body class="blog <?php echo ($dh->canRead()) ? "logged_in" : ""; ?>">
+<body class="blog <?=($dh->canRead()) ? "logged_in" : ""; ?>">
 <?php $this->inc('elements/navbar.php'); ?>
 	<div id="central">
-    <header <?php echo is_object($headImage) ? 'style="background-image:url('.$headImage->getURL() .')"' : "" ?>>
-      <?php  $ai = new Area('Blog Post Header'); $ai->display($c); ?>
-      <h1><?php  echo $c->getCollectionName(); ?></h1>
-      <p class="description"><?php echo $c->getCollectionDescription(); ?></p>
-      <p class="meta"><?php 
-        echo t('%s <em>on</em> <strong>%s</strong>',
-          $c->getVersionObject()->getVersionAuthorUserName(),
-          $c->getCollectionDatePublic(DATE_APP_GENERIC_MDY_FULL));
-      ?></p>	
+    <header <?= is_object($headImage) ? "style='background-image:url('{$headImage->getURL()}')'" : "" ?>>
+      <?php $ai = new Area('Blog Post Header'); $ai->display($c); ?>
+      <h1><?=$c->getCollectionName(); ?></h1>
+      <p class="description"><?=$c->getCollectionDescription(); ?></p>
+      <p class="meta"><?="{$c->getVersionObject()->getVersionAuthorUserName()} <em>on</em> <strong>{$c->getCollectionDatePublic(DATE_APP_GENERIC_MDY_FULL)}</strong>"?></p>	
     </header>
 		<div id="body">
       <article>
-				<?php  $as = new Area('Main'); $as->display($c); ?>
+				<?php $as = new Area('Main'); $as->display($c); ?>
       </article>
 		</div>
 	</div>
