483a7d0a4a741ab908153e9b6ad0d7359ae36204
Joshua Koudys <Joshua Koudys>
2013-12-20 00:14:39 -0500

    New footer

--- themes/janeswalk/elements/footer.php
+++ themes/janeswalk/elements/footer.php
@@ -1,15 +1,12 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
   <footer class="full" role="contentinfo">
     <div class="container">
-      <div class="row-fluid">
-        <div class="span4">  
-          <h4>Connect with us:</h4>
-          <address><p>CSI Annex Building<br /> 720 Bathurst Street, 2nd floor<br> Toronto, ON M5S 2R4<br> 416-642-5779<br><a href="mailto:info@janeswalk.net"> Comments or Questions?&nbsp;</a></p></address>
-        </div>
-        <div class="span8">
-          © 2013 <a href="http://janeswalk.net/">Jane's Walk</a>. All rights reserved. <a href="http://janeswalk.net/index.php/privacy-policy/">Privacy Policy</a><br>
-          <p>Jane's Walk is a project of <a href="http://tidescanada.org/" target="_blank">Tides Canada</a> Initiatives Society.</p>
-        </div>
+      <div class="social-icons">
+        <a href="http://twitter.com/janeswalk"><i class="icon-twitter"></i></a>
+        <a href="http://facebook.com/janeswalk"><i class="icon-facebook-sign"></i></a>
+      </div>
+      <div>
+        <?php $ah = new GlobalArea('Footer'); $ah->display($c); ?>
       </div>
     </div>
   </footer>
