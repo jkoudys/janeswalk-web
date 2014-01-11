123e435095aa3040479dbdc20e05b92d768e80d1
Joshua Koudys <Joshua Koudys>
2014-01-10 23:44:35 -0500

    whitespace cleanup

--- themes/janeswalk/full.php
+++ themes/janeswalk/full.php
@@ -2,16 +2,16 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $dh = Loader::helper('concrete/dashboard');
 $this->inc('elements/header.php'); ?>
-	
+
 <body class="full <?php echo ($dh->canRead()) ? "logged_in" : ""; ?> <?php echo $c->isEditMode() ? "c5-edit" : "" ?>">
   <?php $this->inc('elements/navbar.php'); ?>
-	<div id="central" class="no-sidebar">
-		<div id="body">	
-			<?php (new Area('Main'))->display($c); ?>
-		</div>
-		<div id="full">	
-			<?php (new Area('Full'))->display($c); ?>
-		</div>
-	</div>
-<?php  $this->inc('elements/footer.php'); ?>
+  <div id="central" class="no-sidebar">
+    <div id="body">	
+      <?php (new Area('Main'))->display($c); ?>
+    </div>
+    <div id="full">	
+      <?php (new Area('Full'))->display($c); ?>
+    </div>
+  </div>
+  <?php $this->inc('elements/footer.php'); ?>
 
