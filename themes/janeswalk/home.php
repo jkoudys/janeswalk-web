41b72ebadb119373f6fe1ebf9c36bbf45ee9c53f
Joshua Koudys <Joshua Koudys>
2014-01-10 23:26:50 -0500

    Cleanup indents, extraneous divs

--- themes/janeswalk/home.php
+++ themes/janeswalk/home.php
@@ -11,52 +11,47 @@ global $u; global $cp;
 
 <body class="home <?=($dh->canRead()) ? "logged_in" : ""; ?>" <?= is_object($headImage) ? "style='background-image:url({$headImage->getURL()});background-size:cover;background-position:50%;'" : "" ?>>
   <?php $this->inc('elements/navbar.php');  ?>
-  <div class="container-outter" role="main">
   <div class="intro full">
-
-  <div class="callouts">
-    <blockquote class="homepage-callout2">
-      <?php (new Area('Intro'))->display($c); ?>
-    </blockquote> 
+    <div class="callouts">
+      <blockquote class="homepage-callout2">
+        <?php (new Area('Intro'))->display($c); ?>
+      </blockquote> 
+    </div>
   </div>
-
-</div>
-<!-- end of .intro -->
-<div class="overlap" id="getinvolved">
-  <ul class="controls">
-    <li>
-      <a class="showButton">Show Map <br /><i class="icon-chevron-down"></i></a>
-      <a class="closeButton" style="display:none">Close Map <br /><i class="icon-chevron-up"></i></a>
-    </li>
-  </ul>
-  <section class="map full">
-    <?php (new Area('Map'))->display($c); ?>
-  </section>
-  <section class="calltoaction full">
-    <?php (new Area('Call to Action'))->display($c); ?>
-  </section>
-</div>
-<section class="blog full">
-  <div>
-    <section class="walkblog">
-      <?php (new Area('Blog Header'))->display($c); ?>
-      <?php (new Area('Blog'))->display($c); ?>
+  <!-- end of .intro -->
+  <div class="overlap" id="getinvolved">
+    <ul class="controls">
+      <li>
+        <a class="showButton">Show Map <br /><i class="icon-chevron-down"></i></a>
+        <a class="closeButton" style="display:none">Close Map <br /><i class="icon-chevron-up"></i></a>
+      </li>
+    </ul>
+    <section class="map full">
+      <?php (new Area('Map'))->display($c); ?>
     </section>
-    <section class="twitter">
-      <h3>Twitter</h3>
-      <?php (new Area('Twitter'))->display($c); ?>
+    <section class="calltoaction full">
+      <?php (new Area('Call to Action'))->display($c); ?>
     </section>
   </div>
-</section>
-  
-</div>
-<script>
-$(document).ready(function() { 
-  $('a[href="http://www.janeswalk.net/early#getinvolved"]').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({scrollTop: $("#getinvolved").offset().top})
-    return false;
+  <section class="blog full">
+    <div>
+      <section class="walkblog">
+        <?php (new Area('Blog Header'))->display($c); ?>
+        <?php (new Area('Blog'))->display($c); ?>
+      </section>
+      <section class="twitter">
+        <h3>Twitter</h3>
+        <?php (new Area('Twitter'))->display($c); ?>
+      </section>
+    </div>
+  </section>
+  <script>
+  $(document).ready(function() { 
+    $('a[href="http://www.janeswalk.net/early#getinvolved"]').click(function(event) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: $("#getinvolved").offset().top})
+        return false;
     });
   });
-</script>
-<?php $this->inc('elements/footer.php');  ?>
+  </script>
+  <?php $this->inc('elements/footer.php');  ?>
