52bde4c9329f952837185f77de56f6121e16d931
Joshua Koudys <Joshua Koudys>
2014-01-24 16:38:05 -0500

    Make the walk list show coloured circles

--- blocks/page_list/templates/show_owner/view.php
+++ blocks/page_list/templates/show_owner/view.php
@@ -2,7 +2,7 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $th = Loader::helper('text');
 $av = Loader::helper('concrete/avatar');
-//$ih = Loader::helper('image'); //<--uncomment this line if displaying image attributes (see below)
+$ih = Loader::helper('image'); //<--uncomment this line if displaying image attributes (see below)
 //Note that $nh (navigation helper) is already loaded for us by the controller (for legacy reasons)
 ?>
 <?php foreach ($pages as $page): ?>
@@ -14,12 +14,18 @@ $av = Loader::helper('concrete/avatar');
   $cities->filterByParentID($page->getCollectionID());
   foreach($cities->get(1000) as $city) {
     $page_owner = UserInfo::getByID($city->getCollectionUserID());
+    /* User 103 is special-cased as someone we don't want to show details for. May remove once data loaded. */
     if($page_owner->getUserID() > 1 && $page_owner->getUserID() != 103 && $page_owner->getAttribute('first_name')) {
     ?>
       <section class="city-organizer">
-        <h3><?=$city->getCollectionName()?></h3>
-        <?php if($avatar = $av->getImagePath($page_owner)) { echo "<div class='u-avatar' style='background-image:url({$avatar})'></div>"; } 
-        else { echo "<div class='u-avatar' style='background-image:url({$this->getThemePath()}/img/walk-organizer.png)'></div>"; } ?>
+        <h3><a href="<?=$nh->getLinkToCollection($page)?>"><?=$city->getCollectionName()?></a></h3>
+        <a href="<?=$nh->getLinkToCollection($page)?>">
+          <?php if($avatar = $av->getImagePath($page_owner)) { ?>
+            <div class='u-avatar' style='background-image:url(<?=$avatar?>)'></div> 
+          <?php } else { ?>
+            <div class='u-avatar placeholder<?=ord($page_owner->getUserID()) % 3?>'></div>
+          <?php } ?>
+        </a>
         <div class="city-organizer-details">
           <?="<h3>{$page_owner->getAttribute('first_name')} {$page_owner->getAttribute('last_name')}</h3><h4>City Organizer</h4>" ?>
           <div class="btn-toolbar">
@@ -35,4 +41,3 @@ $av = Loader::helper('concrete/avatar');
   }
 echo '</ul>';
 endforeach; ?>
-
