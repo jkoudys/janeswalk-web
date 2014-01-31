5cb7c6fcb96fd7573de2625b520f6ec22d74b3c2
Joshua Koudys <Joshua Koudys>
2014-01-31 17:38:26 -0500

    take area vars out of scope

--- themes/janeswalk/blog_entry.php
+++ themes/janeswalk/blog_entry.php
@@ -11,7 +11,7 @@ $ui = UserInfo::getByID($c->getCollectionUserID());
 	<div id="central">
     <header <?= is_object($headImage) ? "style='background-image:url({$headImage->getURL()})'" : "" ?>>
       <i class="backfade"></i>
-      <?php $ai = new Area('Blog Post Header'); $ai->display($c); ?>
+      <?php (new Area('Blog Post Header'))->display($c); ?>
       <h1><?=$c->getCollectionName(); ?></h1>
       <p class="description"><?=$c->getCollectionDescription(); ?></p>
       <?php if($ui) { ?><p class="meta"><?= ($first_name = $ui->getAttribute('first_name')) ? ("$first_name {$ui->getAttribute('last_name')}") : $ui->getUserObject()->getUserName()?>, <strong><?= $c->getCollectionDatePublic(DATE_APP_GENERIC_MDY_FULL)?></strong></p>	<?php } ?>
@@ -20,7 +20,7 @@ $ui = UserInfo::getByID($c->getCollectionUserID());
       <article>
 				<?php 
         if (is_object(ComposerPage::getByID($c->getCollectionID()))) { echo "<a href='{$this->url('/dashboard/composer/write/-/edit/' . $c->getCollectionID())}' style='margin-bottom:1em;display:block'><i class='icon-edit-sign'></i> edit</a>"; }
-        $as = new Area('Main'); $as->display($c); ?>
+        (new Area('Main'))->display($c); ?>
       </article>
       <div class="well" style="clear: both;">
         <div id="disqus_thread"></div>
