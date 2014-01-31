abc91d6a43e8bf83bb12960bae184f16122ecbc4
Joshua Koudys <Joshua Koudys>
2014-01-31 17:35:24 -0500

    fix up ugly code

--- themes/janeswalk/city_blog.php
+++ themes/janeswalk/city_blog.php
@@ -1,17 +1,13 @@
 <?php 
 defined('C5_EXECUTE') or die("Access Denied.");
-$page = Page::getCurrentPage();
 $nh = Loader::helper('navigation');
-$parentPage = Page::getByID( $page->getCollectionParentID() );
+$parentPage = Page::getByID( $c->getCollectionParentID() );
 $this->inc('elements/header.php'); ?>
 	<div id="central" class="no-sidebar">
 		
 		<div id="body">	
-			<?echo "<a href='{$nh->getCollectionURL($parentPage)}'>Back to {$parentPage->getCollectionName()} Walks</a><h2>{$parentPage->getCollectionName()} Walk Blog</h2>"; 
-			$a = new Area('Main');
-			$a->display($c);
-			
-			?>
+      <a href='<?=$nh->getCollectionURL($parentPage)?>'>Back to <?=$parentPage->getCollectionName()?> Walks</a><h2><?=$parentPage->getCollectionName()?> Walk Blog</h2>
+      <?php (new Area('Main'))->display($c); ?>
 		</div>
 	</div>
 <?php  $this->inc('elements/footer.php'); ?>
