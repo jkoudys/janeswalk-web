55563f4f082cac39eedf3c652159c2832f465ada
Joshua Koudys <Joshua Koudys>
2014-02-10 10:35:06 -0500

    Cleanup indentation

--- models/attribute/types/page/controller.php
+++ models/attribute/types/page/controller.php
@@ -3,54 +3,54 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 class PageAttributeTypeController extends AttributeTypeController  {
 
-	protected $searchIndexFieldDefinition = 'I DEFAULT 0 NULL';
+  protected $searchIndexFieldDefinition = 'I DEFAULT 0 NULL';
 
-	public function getValue() {
-		$db = Loader::db();
-		$value = $db->GetOne("select cID from atPage where avID = ?", array($this->getAttributeValueID()));
-		if ($value > 0) {
-			$p = Page::getByID($value);
-			return $p;
-		}
-	}
-	
-	public function getDisplayValue() {
+  public function getValue() {
+    $db = Loader::db();
+    $value = $db->GetOne("select cID from atPage where avID = ?", array($this->getAttributeValueID()));
+    if ($value > 0) {
+      $p = Page::getByID($value);
+      return $p;
+    }
+  }
+
+  public function getDisplayValue() {
     $nh = Loader::helper('navigation');
-		$p = $this->getValue();
-		if (is_object($p)) {
-			return "<a href='{$nh->getCollectionURL($p)}'>{$p->getCollectionName()}</a>";
-		}
-	}
+    $p = $this->getValue();
+    if (is_object($p)) {
+      return "<a href='{$nh->getCollectionURL($p)}'>{$p->getCollectionName()}</a>";
+    }
+  }
 
-	public function getDisplaySanitizedValue() {
-		return $this->getDisplayValue();
-	}
-	
-	public function searchForm($list) {
-		$cID = $this->request('value');
-		$list->filterByAttribute($this->attributeKey->getAttributeKeyHandle(), $cID);
-		return $list;
-	}
-	
-	public function getSearchIndexValue() {
-		$db = Loader::db();
-		$value = $db->GetOne("select cID from atPage where avID = ?", array($this->getAttributeValueID()));
-		return $value;	
-	}
-	
-	public function importValue(SimpleXMLElement $akv) {
-		if (isset($akv->value->cID)) {
-			$cIDVal = (string) $akv->value->cID;
-			$cID = ContentImporter::getValue($cIDVal);
-			if ($cID) {
-				return Page::getByID($cID);
-			}
-		}
-	}
-	
-	public function search() {
-	}
-	
+  public function getDisplaySanitizedValue() {
+    return $this->getDisplayValue();
+  }
+
+  public function searchForm($list) {
+    $cID = $this->request('value');
+    $list->filterByAttribute($this->attributeKey->getAttributeKeyHandle(), $cID);
+    return $list;
+  }
+
+  public function getSearchIndexValue() {
+    $db = Loader::db();
+    $value = $db->GetOne("select cID from atPage where avID = ?", array($this->getAttributeValueID()));
+    return $value;	
+  }
+
+  public function importValue(SimpleXMLElement $akv) {
+    if (isset($akv->value->cID)) {
+      $cIDVal = (string) $akv->value->cID;
+      $cID = ContentImporter::getValue($cIDVal);
+      if ($cID) {
+        return Page::getByID($cID);
+      }
+    }
+  }
+
+  public function search() {
+  }
+
   public function form() {
     Loader::model('page_list');
     $pl = new PageList();
@@ -67,12 +67,12 @@ class PageAttributeTypeController extends AttributeTypeController  {
       $parent = Page::getByID($page->getCollectionParentID())->getCollectionName();
       if($lastParent != $parent) {
         if($lastParent != "") { $selectString .= '</optgroup>'; }
-        $selectString .= "<optgroup label='$parent'>";
-        $lastParent = $parent;
+      $selectString .= "<optgroup label='$parent'>";
+      $lastParent = $parent;
       }
       $selectedAttributeVal = '';
       if ($this->getValue()->cID === $page->cID) {
-          $selectedAttributeVal = ' selected="selected"';
+        $selectedAttributeVal = ' selected="selected"';
       }
       $selectString .= "<option value=\"{$page->getCollectionID()}\"" . ($selectedAttributeVal) . ">{$page->getCollectionName()}</option>";
     }
@@ -80,35 +80,35 @@ class PageAttributeTypeController extends AttributeTypeController  {
     print $selectString;
   }
 
-	// run when we call setAttribute(), instead of saving through the UI
-	public function saveValue($obj) {
-		$db = Loader::db();
-		if (is_object($obj) && (!$obj->isError())) {
-			$db->Replace('atPage', array('avID' => $this->getAttributeValueID(), 'cID' => $obj->getCollectionID()), 'avID', true);
-		}
-	}
-	
-	public function deleteKey() {
-		$db = Loader::db();
-		$arr = $this->attributeKey->getAttributeValueIDList();
-		foreach($arr as $id) {
-			$db->Execute('delete from atPage where avID = ?', array($id));
-		}
-	}
-	
-	public function saveForm($data) {
-		if ($data['value'] > 0) {
-			$p = Page::getByID($data['value']);
-			$this->saveValue($p);
-		} else {
-			$db = Loader::db();
-			$db->Replace('atPage', array('avID' => $this->getAttributeValueID(), 'cID' => 0), 'avID', true);	
-		}
-	}
-	
-	public function deleteValue() {
-		$db = Loader::db();
-		$db->Execute('delete from atPage where avID = ?', array($this->getAttributeValueID()));
-	}
-	
+  // run when we call setAttribute(), instead of saving through the UI
+  public function saveValue($obj) {
+    $db = Loader::db();
+    if (is_object($obj) && (!$obj->isError())) {
+      $db->Replace('atPage', array('avID' => $this->getAttributeValueID(), 'cID' => $obj->getCollectionID()), 'avID', true);
+    }
+  }
+
+  public function deleteKey() {
+    $db = Loader::db();
+    $arr = $this->attributeKey->getAttributeValueIDList();
+    foreach($arr as $id) {
+      $db->Execute('delete from atPage where avID = ?', array($id));
+    }
+  }
+
+  public function saveForm($data) {
+    if ($data['value'] > 0) {
+      $p = Page::getByID($data['value']);
+      $this->saveValue($p);
+    } else {
+      $db = Loader::db();
+      $db->Replace('atPage', array('avID' => $this->getAttributeValueID(), 'cID' => 0), 'avID', true);	
+    }
+  }
+
+  public function deleteValue() {
+    $db = Loader::db();
+    $db->Execute('delete from atPage where avID = ?', array($this->getAttributeValueID()));
+  }
+
 }
