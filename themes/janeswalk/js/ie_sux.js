a195ec536d05dd4c5760470a852ebae3bae5600e
Joshua Koudys <Joshua Koudys>
2014-03-12 11:45:57 -0400

    bg images now stretchy on ie8 - though still ugly

--- themes/janeswalk/js/ie_sux.js
+++ themes/janeswalk/js/ie_sux.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       img.setAttribute("src", bgUrl);
       img.setAttribute("width", "100%");
       img.setAttribute("height", "100%");
-      img.setAttribute("style", "position:fixed;top:0;left:0");
+      img.setAttribute("style", "position:fixed;top:0;left:0;height:100%;width:100%;z-index:-1");
       document.querySelector("body").appendChild(img);
     }).call(this);
     $('.backfade').remove();
