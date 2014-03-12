fc63ec7957fcc48d134348beab55c2d6f1a1ce4e
Joshua Koudys <Joshua Koudys>
2014-03-12 11:40:59 -0400

    IE fix to scale bg images

--- themes/janeswalk/js/ie_sux.js
+++ themes/janeswalk/js/ie_sux.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
   if (window.PIE) {
-    $('body.city-page, i[class^=icon-], .u-avatar').each(function() {
+    (function() {
+      var bgUrl = /^url\((['"]?)(.*)\1\)$/.exec($('body.city-page').css('background-image'));
+      var img = document.createElement("img");
+      bgUrl = bgUrl ? bgUrl[2] : false;
+      img.setAttribute("src", bgUrl);
+      img.setAttribute("width", "100%");
+      img.setAttribute("height", "100%");
+      img.setAttribute("style", "position:fixed;top:0;left:0");
+      document.querySelector("body").appendChild(img);
+    }).call(this);
+    $('.backfade').remove();
+
+    $('i[class^=icon-], .u-avatar').each(function() {
       PIE.attach(this);
     });
-    $('.backfade').remove();
   }
 });
