5e3042af923edd31ddbf24d9b4929a5df523c580
Oliver Nassar <onassar@gmail.com>
2014-03-13 15:14:41 -0400

    Cache bust fix

--- themes/janeswalk/elements/footer.php
+++ themes/janeswalk/elements/footer.php
@@ -46,12 +46,12 @@
   <!-- v2 (onassar) -->
   <script src="<?=$this->getThemePath()?>/js/app.js" type="text/javascript"></script>
   <script src="<?=$this->getThemePath()?>/js/v2/extend.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/FacebookShareDialog.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/View.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/views/Page.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/Home.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/City.js" type="text/javascript"></script>
-  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/Walk.js" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/FacebookShareDialog.js?1" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/View.js?1" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/views/Page.js?1" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/Home.js?1" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/City.js?1" type="text/javascript"></script>
+  <script src="<?=$this->getThemePath()?>/js/v2/views/pages/Walk.js?1" type="text/javascript"></script>
 
   <?php Loader::element('footer_required'); ?>
 </body>
