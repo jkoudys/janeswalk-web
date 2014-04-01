2787c309b31be2142c124b2177718463ef03a7cf
Joshua Koudys <Joshua Koudys>
2014-04-01 12:13:59 -0400

    Don't try to load city info if not profile owner

--- controllers/profile/controller.php
+++ controllers/profile/controller.php
@@ -8,52 +8,55 @@ class ProfileController extends Concrete5_Controller_Profile {
     $u = new User();
     $ui = UserInfo::getByID($u->getUserID());
     $profile = $this->get('profile');
-
-    $pl = new PageList();
-    $pl->filterByCollectionTypeHandle('walk');
-    $pl->filterByUserID($u->getUserID());
-    $pl->filterByAttribute('exclude_page_list',false);
-    $this->set('publicWalks', $pl->get());
-
-    $pl = new PageList();
-    $pl->filterByCollectionTypeHandle('walk');
-    $pl->filterByUserID($u->getUserID());
-    $pl->filterByAttribute('exclude_page_list',true);
-    $this->set('inProgressWalks', $pl->get());
-
-    $cities = new PageList();
-    $cities->filterByCollectionTypeHandle('city');
-    $cities->sortByName();
-    $this->set('cities', $cities->get());
-
     $isProfileOwner = $u->getUserID() == $profile->getUserID();
-    $isCityOrganizer = in_array('City Organizers', $profile->getUserObject()->getUserGroups());
-    if($isCityOrganizer && $isProfileOwner) {
+
+    if($isProfileOwner) {
       $pl = new PageList();
-      $pl->filterByCollectionTypeHandle('city');
-      $pl->ignoreAliases();
+      $pl->filterByCollectionTypeHandle('walk');
       $pl->filterByUserID($u->getUserID());
-      $cityWalks = [];
-      foreach($pl->get() as $city) {
+      $pl->filterByAttribute('exclude_page_list',false);
+      $this->set('publicWalks', $pl->get());
+
+      $pl = new PageList();
+      $pl->filterByCollectionTypeHandle('walk');
+      $pl->filterByUserID($u->getUserID());
+      $pl->filterByAttribute('exclude_page_list',true);
+      $this->set('inProgressWalks', $pl->get());
+
+      $cities = new PageList();
+      $cities->filterByCollectionTypeHandle('city');
+      $cities->sortByName();
+      $this->set('cities', $cities->get());
+
+      $isCityOrganizer = in_array('City Organizers', $profile->getUserObject()->getUserGroups());
+      if($isCityOrganizer) {
         $pl = new PageList();
-        $pl->filterByCollectionTypeHandle('walk');
-        $pl->filterByParentID($city->getCollectionID());
-        $pl->filterByAttribute('exclude_page_list',false);
-        $walks = $pl->get();
-        $pl = new PageList();
-        $pl->filterByCollectionTypeHandle('walk');
-        $pl->filterByParentID($city->getCollectionID());
-        $pl->filterByAttribute('exclude_page_list',true);
-        $cityWalks[] = ['city' => $city, 'walks' => $walks, 'inprogress' => $pl->get()];
+        $pl->filterByCollectionTypeHandle('city');
+        $pl->ignoreAliases();
+        $pl->filterByUserID($u->getUserID());
+        $cityWalks = [];
+        foreach($pl->get() as $city) {
+          $pl = new PageList();
+          $pl->filterByCollectionTypeHandle('walk');
+          $pl->filterByParentID($city->getCollectionID());
+          $pl->filterByAttribute('exclude_page_list',false);
+          $walks = $pl->get();
+          $pl = new PageList();
+          $pl->filterByCollectionTypeHandle('walk');
+          $pl->filterByParentID($city->getCollectionID());
+          $pl->filterByAttribute('exclude_page_list',true);
+          $cityWalks[] = ['city' => $city, 'walks' => $walks, 'inprogress' => $pl->get()];
+        }
+        $this->set('cityWalks', $cityWalks);
       }
-      $this->set('cityWalks', $cityWalks);
+
+      $this->set('home_city', $ui->getAttribute('home_city'));
     }
 
     $this->set('isProfileOwner', $isProfileOwner);
     $this->set('isCityOrganizer', $isCityOrganizer);
     $this->set('nh',$nh);
     $this->set('u',$u);
-    $this->set('home_city', $ui->getAttribute('home_city'));
     $this->set('newWalkForm', Page::getByPath('/walk/form'));
   }
 }
