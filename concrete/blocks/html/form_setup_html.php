31bff3fb85f2503324939a22504345e2cd0ff610
Joshua Koudys <Joshua Koudys>
2014-03-31 23:13:56 -0400

    Stop tracking the concrete dir

--- concrete/blocks/html/form_setup_html.php
+++
@@ -1,3 +0,0 @@
-<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>  
-
-<textarea id="ccm-HtmlContent" name="content" style="width: 98%; height: 440px"><?php echo htmlentities($controllerObj->content, ENT_COMPAT, APP_CHARSET) ?></textarea>
