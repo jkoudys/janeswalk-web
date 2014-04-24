ecaa93254ba62e4788fe0360b220ee72afe4e999
Oliver Nassar <onassar@gmail.com>
2014-04-24 13:27:44 -0400

    new image

--- themes/janeswalk/city.php
+++ themes/janeswalk/city.php
@@ -43,7 +43,7 @@
 <?php
   $donateCopyOptions = array(
     array(
-      'imagePath' => 'http://i.imgur.com/nEBJJQq.jpg',
+      'imagePath' => 'http://i.imgur.com/H478xwO.jpg',
       'main' => t('Help Jane\'s Walk run more than 1,000 walks this year'),
       'cta' => t('Text %1$s to %2$s to donate %3$s', 'JANE', '45678', '$10')
     )
