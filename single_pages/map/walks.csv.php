3d5489194a82554db208f38fe41369708e7df13f
Joshua Koudys <Joshua Koudys>
2014-04-29 14:47:46 -0400

    add two new ways to create walk maps

---
+++ single_pages/map/walks.csv.php
@@ -0,0 +1,44 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$pl = new PageList();
+$nh = Loader::helper('navigation');
+$pl->filterByCollectionTypeHandle('walk');
+$pages = $pl->get();
+
+$walks = array();
+foreach($pages as $page) {
+  // Creates a coordinates element and gives it the value of the lng and lat columns from the results.
+  $gmap = json_decode($page->getAttribute('gmap'),true);
+  $coordinates = false;
+  $name = $page->getCollectionName();
+  if(!($name && isset($gmap['markers']))) { continue; } // Don't bother if there's no coordinates or name; blank walk
+  $city = Page::getByID($page->getCollectionParentID());
+  $country = Page::getByID($city->getCollectionParentID());
+  // Since we're generating csv, set keys here to enforce ordering
+  $walk = array(
+    'id' => $page->getCollectionID(),
+    'map_id' => $page->getCollectionID(),
+    'title' => '"' . htmlspecialchars($name) . '"',
+    'description' => '"' . htmlspecialchars($page->getAttribute('shortdescription') . '<br />' . "<a target=\"__blank\" href='{$nh->getCollectionURL($page)}'>Go to walk &gt;</a>" ) . '"',
+    'link' => $nh->getCollectionURL($page),
+    'icon' => '',
+    'lat' => '',
+    'lng' => ''
+  );
+  foreach( (array) $gmap['markers'] as $marker) { // To avoid errors on empty/malformed maps
+    $walk['lat'] = $marker['lat'];
+    $walk['lng'] = $marker['lng'];
+    break; 
+  }
+  $walks[] = $walk;
+}
+
+header('Content-type: text/csv');
+if(count($walks)) {
+  // Display the csv header
+  echo join(',', array_keys($walks[0])), PHP_EOL;
+  foreach($walks as $walk) {
+    echo join(',', $walk), PHP_EOL;
+  }
+}
+exit;
---
+++ single_pages/map/walks.json.php
@@ -0,0 +1,62 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied."); 
+
+$pl = new PageList();
+$nh = Loader::helper('navigation');
+$pl->filterByCollectionTypeHandle('Walk');
+$pages = $pl->get();
+$geo = [
+  'type' => 'FeatureCollection',
+  'features' => []
+  ];
+foreach($pages as $walk) {
+  $parent = Page::getByID($walk->getCollectionParentID());
+  $walk_owner = UserInfo::getByID($walk->getCollectionUserID());
+  $city = t($city_name = $parent->getCollectionName());
+  foreach((array) json_decode((string) $walk->getAttribute('gmap'), true)['markers'] as $marker) {
+    $geo['features'][] = [
+      'type' => 'feature',
+      'geometry' => [
+        'type' => 'Point',
+        'coordinates' => [ $marker['lat'], $marker['lng'] ]
+      ]
+    ];
+    break;
+  }
+}
+echo json_encode($geo);
+exit;
+
+/*
+{
+              "type": "FeatureCollection", 
+              "features": [
+                {"geometry": {
+                    "type": "GeometryCollection", 
+                    "geometries": [
+                        {
+                            "type": "LineString", 
+                            "coordinates": 
+                                [[11.0878902207, 45.1602390564], 
+                                [15.01953125, 48.1298828125]]
+                        }, 
+                        {
+                            "type": "Polygon", 
+                            "coordinates": 
+                                [[[11.0878902207, 45.1602390564], 
+                                  [14.931640625, 40.9228515625], 
+                                  [0.8251953125, 41.0986328125], 
+                                  [7.63671875, 48.96484375], 
+                                  [11.0878902207, 45.1602390564]]]
+                        },
+                        {
+                            "type":"Point", 
+                            "coordinates":[15.87646484375, 44.1748046875]
+                        }
+                    ]
+                }, 
+                "type": "Feature", 
+                "properties": {}}
+              ]
+}
+ */
