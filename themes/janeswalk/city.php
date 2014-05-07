fad36d00c96bef6d37fb88e2008854c2c446264c
Oliver Nassar <onassar@gmail.com>
2014-05-07 16:38:58 -0400

    New donate modal

--- themes/janeswalk/city.php
+++ themes/janeswalk/city.php
@@ -43,7 +43,7 @@
 <?php
   $donateCopyOptions = array(
     array(
-      'imagePath' => 'http://i.imgur.com/zv12rlD.jpg',
+      'imagePath' => 'http://i.imgur.com/GHiFhvs.png',
       'main' => t('Help Jane\'s Walk support more than 1,000 walks'),
       'cta' => t('Text %1$s to %2$s to donate %3$s', 'JANE', '45678', '$10')
     )
