b26909801669639bd91e38cdd69fee0ccfd54075
Joshua Koudys <Joshua Koudys>
2014-06-18 15:41:02 -0400

    Allow dates to be removed

--- models/attribute/types/event_time/form.php
+++ models/attribute/types/event_time/form.php
@@ -1,8 +1,27 @@
 <style>
 .dateList input, .dateList select { display:inline-block !important; }
-ul.dateList { margin-left:0; }
+ul.dateList { margin-left:0; position:relative; }
+ul.dateList .ccm-add-option { position:absolute; top:-25px; right:5px; display:none; }
 ul.dateList legend { margin-bottom:5px; }
+ul.dateList fieldset { position:relative; }
+ul.dateList fieldset .ccm-search-remove-option { position:absolute; top:0; right:2px; }
+ul.dateList fieldset .ccm-search-remove-option:hover { cursor:pointer; }
 </style>
+<script>
+var closeField = function() {
+  var eventdate = this.parentNode;
+  eventdate.parentNode.removeChild(eventdate);
+  alert("foo");
+};
+
+function EventDate(akID) {
+  var dateList = document.getElementById("ak" + akID);
+  var dates = dateList.querySelectorAll("fieldset .ccm-search-remove-option");
+  for(var i = 0, e = dates[i]; e; i++, e = dates[i]) {
+    e.addEventListener("click", closeField);
+  }
+}
+</script>
 
 <label class="checkbox">
   <?= $fh->checkbox("akID[$akID][open]", 'open', $data['open']) ?>
@@ -13,9 +32,12 @@ ul.dateList legend { margin-bottom:5px; }
 <?= $fh->select("akID[$akID][type]", ['' => 'No Booking', 'set' => 'Scheduled', 'all' => 'By Request'], $data['type'] ) ?>
 </label>
 <ul class="dateList" id="ak<?= $akID ?>_eventTimeList">
+  <a class="ccm-add-option"><img src="<?= ASSETS_URL_IMAGES ?>/icons/add.png" /></a>
 <?php
 if($data['slots']) foreach($data['slots'] as $key=>$slot) { ?>
+  <script>new EventDate(<?= $akID ?>);</script>
   <fieldset>
+    <a class="ccm-search-remove-option"><img src="<?= ASSETS_URL_IMAGES ?>/icons/remove_minus.png" /></a>
     <legend>Date <?=$key?></legend>
     <label class='datetime'>
       Start<br/>
