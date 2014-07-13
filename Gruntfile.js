ba1c5a1544dde9ef1d1f371c8c3a5f9b28253505
Joshua Koudys <jkoudys@gmail.com>
2014-07-13 11:58:08 -0400

    Use gruntjs for task running!

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    themes: {
+      janeswalk: {
+        js: "themes/janeswalk/js/"
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= themes.janeswalk.js + pkg.name %>.js',
+        dest: '<%= themes.janeswalk.js + pkg.name %>.min.js',
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['<%= themes.janeswalk.js %>/v2/**/*.js', "<%= themes.janeswalk.js %>/app.js"],
+        // the location of the resulting JS file
+        dest: '<%= themes.janeswalk.js + pkg.name %>.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
