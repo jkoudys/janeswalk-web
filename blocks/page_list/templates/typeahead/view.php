36c327e5f6aa757faca4fa6ed4db490ff0865cf1
Joshua Koudys <jkoudys@gmail.com>
2014-07-24 18:39:51 -0400

    Use XSLT for templating

--- blocks/page_list/templates/typeahead/view.php
+++ blocks/page_list/templates/typeahead/view.php
@@ -8,83 +8,29 @@ $th = Loader::helper('text');
  * just build a DOMDocument instead, to manage the control logic nicely
  */
 $doc = new DOMDocument;
-$typeahead = $doc->appendChild($doc->createElement('div'));
-$typeahead->setAttribute('class', 'ccm-page-list-typeahead');
 
-// The whole <fieldset class="search"> box
-$search = $typeahead->appendChild($doc->createElement('form'))->appendChild($doc->createElement('fieldset'));
-$search->setAttribute('class', 'search');
-
-// The search field
-$inputSearch = $search->appendChild($doc->createElement('input'));
-$inputSearch->setAttribute('type', 'text');
-$inputSearch->setAttribute('name', 'selected_option');
-$inputSearch->setAttribute('class', 'typeahead');
-$inputSearch->setAttribute('placeholder', 'Find a Walk in which city?');
-$inputSearch->setAttribute('autocomplete', 'off');
-
-// Submit button
-$inputSubmit = $search->appendChild($doc->createElement('input'));
-$inputSubmit->setAttribute('type', 'submit');
-$inputSubmit->setAttribute('value', 'Go');
-
-// The dropdown list of countries/cities
-$ul = $search->appendChild($doc->createElement('ul'));
-
-/**
- * @var array[int id, string name] $countryNames
- * We were loading a _lot_ of countries, and it was slowing things down to grab
- * their Page object, and their name, 50k+ times in the sort + building the list.
- */
-$countryNames = array();
-
-// Sort city pages based on their country
-uasort(
-    $pages,
-    function ($a,$b)
-    {
-        // Grab each city's country
-        $ap = $a->getCollectionParentID();
-        $bp = $b->getCollectionParentID();
-        if ($ap === $bp) {
-            // If they're in the same country, sort by city name
-            return strcmp($a->getCollectionName(), $b->getCollectionName());
-        } else {
-            // Otherwise, sort into their country
-            if(!isset($countryNames[$ap])) $countryNames[$ap] = Page::getByID($ap)->getCollectionName();
-            if(!isset($countryNames[$bp])) $countryNames[$bp] = Page::getByID($bp)->getCollectionName();
-            return strcmp($countryNames[$ap], $countryNames[$bp]);
-        }
+$countryList = array();
+$countries = $doc->appendChild($doc->createElement('countries'));
+foreach ($pages as $city) {
+    $pcID = (int) $city->getCollectionParentID();
+    if (!isset($countryList[$pcID])) {
+        $countryList[$pcID] = $countries->appendChild(
+            $doc->createElement(
+                'country',
+                Page::getByID($pcID)->getCollectionName()
+            )
+        );
     }
-);
-
-// Show the country parent list and city pages
-$lastCountryID = false;
-foreach ($pages as $cityPage) {
-    $countryID = Page::getByID($cityPage->getCollectionParentID())->getCollectionID();
-    if ($countryID !== $lastCountryID) {
-        $li = $ul->appendChild($doc->createElement('li'));
-        $li->setAttribute('class', 'country');
-        $div = $li->appendChild($doc->createElement('div', $countryNames[$countryID]));
-        $div->setAttribute('class', 'name');
-        $cityList = $ul->appendChild($doc->createElement('ul'));
-        $cityList->setAttribute('class', 'cities');
-        $lastCountryID = $countryID;
-    }
-    $city = $cityList->appendChild($doc->createElement('li'));
-    $a = $city->appendChild($doc->createElement('a', $th->entities($cityPage->getCollectionName())));
-    $a->setAttribute('href', $nh->getLinkToCollection($cityPage));
+    $cityEl = $countryList[$pcID]->appendChild($doc->createElement('city', $city->getCollectionName()));
+    $cityEl->setAttribute('href', $nh->getLinkToCollection($city));
 }
 
-// Put the add new city option last
-$li = $ul->appendChild($doc->createElement('li'));
-$li->setAttribute('class', 'hidden aux');
-$a = $li->appendChild($doc->createElement('a'));
-// FIXME: Don't use a hard-coded URL here; grab it from the Page
-$a->setAttribute('href', '/city-organizer-onboarding');
-$a->appendChild($doc->createTextNode('Add '));
-$span = $a->appendChild($doc->createElement('span'));
-$span->setAttribute('rel', 'city.name');
-$a->appendChild($doc->createTextNode(' to Jane\'s Walk'));
+// Load the XSL
+$xsl = new XSLTProcessor;
+$xsl->importStyleSheet(DOMDocument::load(__DIR__ . '/view.xsl'));
 
-$doc->saveHTMLFile('php://output');
+echo $doc->saveXML();
+
+// Apply stylesheet and echo it out.
+$xsl->transformToDoc($doc)->saveHTMLFile('php://output');
+
---
+++ blocks/page_list/templates/typeahead/view.xsl
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:template match="countries">
+        <div class="ccm-page-list-typeahead">
+            <form class="search">
+                <input type="text" name="selected_option" class="typeahead" placeholder="Find a Walk in which city?" autocomplete="off" />
+                <input type="submit" value="Go" />
+                <ul>
+                    <xsl:for-each select="country">
+                        <li class="country">
+                            <xsl:value-of select="text()" />
+                            <ul class="cities">
+                                <xsl:for-each select="city">
+                                    <li>
+                                        <a href="{@href}">
+                                            <xsl:value-of select="text()" />
+                                        </a>
+                                    </li>
+                                </xsl:for-each>
+                            </ul>
+                        </li>
+                    </xsl:for-each>
+                    <li class="hidden aux">
+                        <a href="/city-organizer-onboarding">Add <span rel="city.name" /> to Jane's Walk</a>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </xsl:template>
+</xsl:stylesheet>
