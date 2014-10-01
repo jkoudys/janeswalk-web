170df21f9653099faaa24ac638f8cc1b63040473
Joshua Koudys <jkoudys@gmail.com>
2014-10-01 13:18:56 -0400

    Use M as shortcut to show dashboard

--- themes/janeswalk/js/janeswalk.js
+++ themes/janeswalk/js/janeswalk.js
@@ -1973,15 +1973,39 @@ document.addEventListener('DOMContentLoaded', function() {
   if (pageViewName) {
     // The pageViewName class gets loaded from the globally-defined class
     // This is a PHP-ish approach to OO, and classes themselves (not their
-    // objects) are the only things that should be declared globally.
-    try {
-      // FIXME: I'm not in-love with such a heavy jQuery reliance
-      new window[pageViewName]($(document.body))
-    } catch(e) {
-      console.log('Error instantiating page view ' + pageViewName + ': ' + e);
-    }
+      // objects) are the only things that should be declared globally.
+      try {
+        // FIXME: I'm not in-love with such a heavy jQuery reliance
+        new window[pageViewName]($(document.body))
+      } catch(e) {
+        console.log('Error instantiating page view ' + pageViewName + ': ' + e);
+      }
   } else {
     console.log('No page view defined.');
+  }
+
+  // Init keyboard shortcuts
+  var toolbar = document.getElementById('ccm-toolbar');
+  if (toolbar) {
+    window.addEventListener("keyup", function(ev) {
+      /* Don't capture inputs going into a form */
+      if(ev.target.tagName !== "INPUT") {
+        ev.preventDefault();
+        switch(String(ev.key || (ev.keyCode && String.fromCharCode(ev.keyCode)) || ev.char).toUpperCase()) {
+          case "M":
+            if (toolbar.style) {
+              if (toolbar.style.zIndex == 99999) {
+                toolbar.style.zIndex = -1;
+              } else {
+                toolbar.style.zIndex = 99999;
+              }
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    });
   }
 
 });
