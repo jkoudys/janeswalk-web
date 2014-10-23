961069f8edbef4f57ce39217b0b770ceba48c118
Joshua Koudys <jkoudys@gmail.com>
2014-10-23 11:01:33 -0400

    Include body data for full page

--- themes/janeswalk/full.php
+++ themes/janeswalk/full.php
@@ -1,10 +1,11 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$dh = Loader::helper('concrete/dashboard');
-$this->inc('elements/header.php'); ?>
 
-<body class="full<?= ($dh->canRead() ? ' logged_in' : '') . ($c->isEditMode() ? ' c5-edit' : '') ?>" data-pageViewName='PageView'>
-  <?php $this->inc('elements/navbar.php'); ?>
+$bodyData = [];
+$bodyData['classes'][] = 'full';
+$this->controller->set('bodyData', $bodyData);
+$this->inc('elements/header.php');
+$this->inc('elements/navbar.php'); ?>
   <div id="central" class="no-sidebar">
     <div id="body">
       <?php (new Area('Main'))->display($c); ?>
