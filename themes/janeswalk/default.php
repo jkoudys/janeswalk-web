cf148b1a90c08c12ed809198313a75844f5d3cb7
Joshua Koudys <jkoudys@gmail.com>
2014-10-30 17:49:46 -0400

    Add navbar to default layout

--- themes/janeswalk/default.php
+++ themes/janeswalk/default.php
@@ -1,21 +1,21 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$this->inc('elements/header.php'); ?>
+$this->inc('elements/header.php');
+$this->inc('elements/navbar.php');
+?>
+<div id="central">
+    <div id="sidebar">
+        <?php
+        $as = new Area('Sidebar');
+        $as->display($c);
+        ?>
+    </div>
 
-	<div id="central">
-		<div id="sidebar">
-			<?php
-            $as = new Area('Sidebar');
-            $as->display($c);
-            ?>
-		</div>
-
-		<div id="body">
-			<?php
-            $a = new Area('Main');
-            $a->display($c);
-            ?>
-		</div>
-	</div>
-
+    <div id="body">
+        <?php
+        $a = new Area('Main');
+        $a->display($c);
+        ?>
+    </div>
+</div>
 <?php  $this->inc('elements/footer.php'); ?>
