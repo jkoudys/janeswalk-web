2cf83fce855d6a8ed0518933dfc53ec125f867dd
Joshua Koudys <jkoudys@gmail.com>
2014-11-03 18:24:12 -0500

    Align footer to grid

--- themes/janeswalk/elements/footer.php
+++ themes/janeswalk/elements/footer.php
@@ -1,26 +1,26 @@
-<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
-    <footer class="full" role="contentinfo">
-      <div class="container">
-        <div class="social-icons">
-          <a href="http://twitter.com/janeswalk" target="_blank"><i class="fa fa-twitter"></i></a>
-          <a href="http://facebook.com/janeswalk" target="_blank"><i class="fa fa-facebook-sign"></i></a>
-        </div>
-        <div>
-          <?php $ah = new GlobalArea('Footer'); $ah->display($c); ?>
-        </div>
-      </div>
+<?php  defined('C5_EXECUTE') || die('Access Denied.') ?>
+    <footer role="contentinfo">
+        <nav>
+            <div class="social-icons">
+                <a href="http://twitter.com/janeswalk" target="_blank"><i class="fa fa-twitter"></i></a>
+                <a href="http://facebook.com/janeswalk" target="_blank"><i class="fa fa-facebook-square"></i></a>
+            </div>
+            <div>
+                <?php $ah = new GlobalArea('Footer'); $ah->display($c); ?>
+            </div>
+        </nav>
     </footer>
     <div id="progress" style="z-index: -1;"></div>
     <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-29278390-1']);
-    _gaq.push(['_trackPageview']);
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-29278390-1']);
+        _gaq.push(['_trackPageview']);
 
-    (function () {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+        (function () {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
     </script>
     <script type="text/javascript" src="//use.typekit.net/lxq4ddc.js"></script>
     <script type="text/javascript">try {Typekit.load();} catch (e) {}</script>
