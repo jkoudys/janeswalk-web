da599766842e30ef060297ccad4c4713e11b2d5b
Joshua Koudys <jkoudys@gmail.com>
2014-12-08 16:42:21 -0500

    Complete rearch and cleanup of walk; separate map

---
+++ themes/janeswalk/js/views/pages/Walk.jsx
@@ -0,0 +1,100 @@
+'use strict';
+var PageView = require('../Page.jsx');
+var FacebookShareDialog = require('../FacebookShareDialog.jsx');
+var WalkMap = require('../elements/WalkMap.jsx');
+
+/**
+ * WalkPageView
+ * 
+ * @extends PageView
+ * 
+ * init
+ * 
+ * @public
+ * @param  jQuery element
+ * @return void
+ */
+var WalkPageView = function(element) {
+  PageView.call(this, element);
+  
+  var mapCanvas = document.getElementById('map-canvas');
+
+  this._addFacebookDialogEvents();
+
+  // Check if there's a map to init first
+  if (mapCanvas) {
+    new WalkMap(JanesWalk.page.gmap, mapCanvas);
+  }
+};
+WalkPageView.prototype = Object.create(PageView.prototype, {
+  /**
+   * _addFacebookDialogEvents
+   * 
+   * @protected
+   * @return    void
+   */
+  _addFacebookDialogEvents: {
+    value: function() {
+      var _this = this;
+      this._element.find('.facebookShareLink').click(function(event) {
+        event.preventDefault();
+        _this.trackEvent('Walk', 'share.attempted', 'facebook');
+        var shareObj = _this._getFacebookDialogObj();
+        (new FacebookShareDialog(shareObj)).show(
+          _this._facebookShareFailed,
+          _this._facebookShareSuccessful
+        );
+      });
+    }
+  },
+
+  /**
+   * _facebookShareFailed
+   * 
+   * @protected
+   * @return    void
+   */
+  _facebookShareFailed: {
+    value: function() {
+      this.trackEvent('Walk', 'share.failed', 'facebook');
+    }
+  },
+
+  /**
+   * _facebookShareSuccessful
+   * 
+   * @protected
+   * @return    void
+   */
+  _facebookShareSuccessful: {
+    value: function() {
+      this.trackEvent('Walk', 'share.successful', 'facebook');
+    }
+  },
+
+  /**
+   * _getFacebookDialogObj
+   * 
+   * @see       http://scotch.io/tutorials/how-to-share-webpages-with-facebook
+   * @see       http://www.local-pc-guy.com/web-dev/facebook-feed-dialog-vs-share-link-dialog
+   * @protected
+   * @return    Object
+   */
+  _getFacebookDialogObj: {
+    value: function() {
+      return {
+        link: JanesWalk.page.url,
+        picture: JanesWalk.page.pictureUrl,
+        name: JanesWalk.page.title,
+        description: JanesWalk.page.description,
+        actions: {
+          name: 'View Jane\'s Walks in ' + (JanesWalk.page.city.name),
+          link: JanesWalk.page.city.url
+        }
+      };
+    }
+  },
+
+});
+
+module.exports = WalkPageView;
