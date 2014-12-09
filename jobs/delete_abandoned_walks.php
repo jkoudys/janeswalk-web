5dc9d07ff1a1a024bb6092b7d6281a1289f69f9d
Joshua Koudys <jkoudys@gmail.com>
2014-12-09 09:53:20 -0500

    Cleanup style of walk delete tool

--- jobs/delete_abandoned_walks.php
+++ jobs/delete_abandoned_walks.php
@@ -4,27 +4,34 @@
 * Responsible for geocoding the cities to lookup their lat/lng
 */
 
-defined('C5_EXECUTE') or die("Access Denied.");
-class DeleteAbandonedWalks extends Job {
-  public $jNotUninstallable=0;
+defined('C5_EXECUTE') || die('Access Denied.');
 
-  public function getJobName() {
+class DeleteAbandonedWalks extends Job
+{
+  public $jNotUninstallable = 0;
+
+  public function getJobName()
+  {
     return t('Delete Abandoned Walks');
   }
 
-  public function getJobDescription() {
+  public function getJobDescription()
+  {
     return t('Checks every walk, and moves all walks with a blank title to the trash.');
   }
 
-  public function run() {
-    $pl = new PageList();
+  public function run()
+  {
+    $pl = new PageList;
     $pl->filterByCollectionTypeHandle('walk');
     $pl->filterByName('', true);
     $pages = $pl->get();
-    $pagecount = sizeof($pages);
-    foreach($pages as $page) {
+    $pagecount = count($pages);
+
+    foreach ($pages as $page) {
       $page->moveToTrash();
     }
-    return "$pagecount " . t2('page','pages',$pagecount) . ' moved to the trash';
+
+    return $pagecount. ' ' . t2('page', 'pages', $pagecount) . ' moved to the trash';
   }
 }
