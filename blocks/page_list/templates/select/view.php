6783d16b4faa2908c36e9ae8ad897fc13af0157f
jkoudys <jkoudys@gmail.com>
2014-12-14 01:07:02 -0500

    Prepare pagelist template for React migration

--- blocks/page_list/templates/select/view.php
+++ blocks/page_list/templates/select/view.php
@@ -1,34 +1,43 @@
 <?php
-defined('C5_EXECUTE') or die("Access Denied.");
-$th = Loader::helper('text');
+defined('C5_EXECUTE') || die('Access Denied.');
 
 /**
  * The original pagelist has some funny mixing of echo w/ inline HTML, as well
  * as mixing logic to put out end-tags. This has been very error-prone, so let's
- * just build a DOMDocument instead, to manage the control logic nicely
+ * just build an Array instead, so we may migrate to React later
  */
-$doc = new DOMDocument;
-
-$countryList = array();
-$countries = $doc->appendChild($doc->createElement('countries'));
+$countryList = [];
 foreach ($pages as $city) {
     $pcID = (int) $city->getCollectionParentID();
     if (!isset($countryList[$pcID])) {
-        $countryList[$pcID] = $countries->appendChild(
-            $doc->createElement(
-                'country',
-                Page::getByID($pcID)->getCollectionName()
-            )
-        );
+        $country = Page::getByID($pcID);
+        $countryList[$pcID] = [
+            'name' => $country->getCollectionName(),
+            'uri' => $nh->getLinkToCollection($country),
+            'cities' => []
+        ];
     }
-    $cityEl = $countryList[$pcID]->appendChild($doc->createElement('city', $city->getCollectionName()));
-    $cityEl->setAttribute('href', $nh->getLinkToCollection($city));
+    $countryList[$pcID]['cities'][] = [
+        'id' => $city->getCollectionID(),
+        'name' => $city->getCollectionName(),
+        'uri' => $nh->getLinkToCollection($city)
+    ];
 }
 
-// Load the XSL
-$xsl = new XSLTProcessor;
-$xsl->importStyleSheet(DOMDocument::load(substr(__FILE__, 0, -3) . 'xsl'));
+uasort($countryList, function($a, $b) {
+    return strcmp($a['name'], $b['name']);
+});
 
-// Apply stylesheet and echo it out.
-$xsl->transformToDoc($doc)->saveHTMLFile('php://output');
-
+// Using uncharacteristically functional-programming as preparation for
+// migration to ReactJS
+echo (
+    '<select class="pageListSelect">' .
+        join(array_map(function($country) { return 
+            '<optgroup label="' . $country['name'] . '">' .
+                join(array_map(function($city) { return
+                    '<option value="' . $city['uri'] . '">' . $city['name'] .'</option>';
+                }, $country['cities'])) .
+            '</optgroup>';
+        }, $countryList)) .
+    '</select>'
+);
