cba70a9c1bde0b45177134c687f7cd57b094ea48
jkoudys <jkoudys@gmail.com>
2015-02-20 11:01:52 -0500

    Prepare accessibilities load for c5 config

--- themes/janeswalk/js/views/elements/CAWAccessibleSelect.jsx
+++ themes/janeswalk/js/views/elements/CAWAccessibleSelect.jsx
@@ -2,31 +2,38 @@ var mixins = require('../functions/mixins.jsx');
 
 var AccessibleSelect = React.createClass({
   displayName: 'AccessibleSelect',
+
   mixins: [mixins.linkedParentState],
 
   render: function() {
+    var _this = this;
     var t = this.props.i18n.translate.bind(this.props.i18n);
+    var options = [
+      {id: 'accessible-familyfriendly', name: t('Family friendly')},
+      {id: 'accessible-wheelchair', name: t('Wheelchair accessible')},
+      {id: 'accessible-dogs', name: t('Dogs welcome')},
+      {id: 'accessible-strollers', name: t('Strollers welcome')},
+      {id: 'accessible-bicycles', name: t('Bicycles welcome')},
+      {id: 'accessible-steephills', name: t('Steep hills')},
+      {id: 'accessible-uneven', name: t('Wear sensible shoes (uneven terrain)')},
+      {id: 'accessible-busy', name: t('Busy sidewalks')},
+      {id: 'accessible-bicyclesonly', name: t('Bicycles only')},
+      {id: 'accessible-lowlight', name: t('Low light or nighttime')},
+      {id: 'accessible-seniors', name: t('Senior Friendly')}
+    ];
 
     return (
-      <fieldset>
+      <fieldset id="accessibilities">
         <legend className="required-legend">{ t('How accessible is this walk?') }</legend>
-        <div className="row">
-          <div className="col-md-6">
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-familyfriendly')} />{ t('Family friendly') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-wheelchair')} />{ t('Wheelchair accessible') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-dogs')} />{ t('Dogs welcome') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-strollers')} />{ t('Strollers welcome') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-bicycles')} />{ t('Bicycles welcome') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-steephills')} />{ t('Steep hills') }</label>
-          </div>
-          <div className="col-md-6">
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-uneven')} />{ t('Wear sensible shoes (uneven terrain)') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-busy')} />{ t('Busy sidewalks') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-bicyclesonly')} />{ t('Bicycles only') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-lowlight')} />{ t('Low light or nighttime') }</label>
-            <label className="checkbox"><input type="checkbox" checkedLink={this.linkParentState('accessible-seniors')} />{ t('Senior Friendly') }</label>
-          </div>
-        </div>
+        <fieldset>
+          {options.map(function(option) {
+            return (
+              <label className="checkbox">
+                <input type="checkbox" checkedLink={_this.linkParentState(option.id)} />{option.name}
+              </label>
+              );
+          })}
+        </fieldset>
       </fieldset>
     );
   }
