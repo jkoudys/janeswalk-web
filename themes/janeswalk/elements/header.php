3e8145234eaa5cb2119112717510c217d382e8c5
jkoudys <jkoudys@gmail.com>
2015-04-22 13:53:28 -0400

    Set body to lang of locale

--- themes/janeswalk/elements/header.php
+++ themes/janeswalk/elements/header.php
@@ -17,6 +17,9 @@
      <script type="text/javascript" src="<?=$this->getThemePath();?>/js/ie_sux.js"></script>
 <![endif]-->
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<?php if (Localization::activeLocale() === 'he') { ?>
+    <link href='//fonts.googleapis.com/css?family=Alef:400,700&subset=hebrew' rel='stylesheet' type='text/css'>
+<?php } ?>
     <link rel="stylesheet" href="<?= $this->getThemePath() ?>/css/main.css">
     <script src="<?= CONCRETE5_ENV === 'dev' ? '//cdnjs.cloudflare.com/ajax/libs/react/0.13.1/react-with-addons.js' : '//cdnjs.cloudflare.com/ajax/libs/react/0.13.1/react-with-addons.min.js' ?>"></script>
 <?php
@@ -25,6 +28,7 @@
     <script type="text/javascript">var CCM_THEME_PATH = '<?= $this->getThemePath() ?>';window.fbAsyncInit = function () {FB.init({appId: '544710848887303', status: true, xfbml: true }); };(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/en_US/all.js'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
 </head>
 <body
+    lang="<?= Localization::activeLocale() ?>"
     class="<?= join((array) $bodyData['classes'], ' ') ?>"
     data-pageViewName="<?= $bodyData['pageViewName'] ?>"
     style="background-image:url(<?= $bodyData['bg'] ?>)"
