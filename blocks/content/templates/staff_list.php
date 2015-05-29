4a4aedb2c4d46e926db8d2bfd747b929f5db78f9
jkoudys <jkoudys@gmail.com>
2015-05-29 08:15:16 -0400

    Allow staff list to take json as an input

--- blocks/content/templates/staff_list.php
+++ blocks/content/templates/staff_list.php
@@ -1,55 +1,81 @@
 <?php
-defined('C5_EXECUTE') or die("Access Denied.");
 Loader::model('user_list');
 $av = Loader::helper('concrete/avatar');
 
-// Filter to show only the staff members, or customize
-// by 'custom template' and choose a block name
-$ul = new UserList;
-$ul->filterByGroup((string) $this->getBlockObject()->getBlockName() ?: 'Staff');
-$ul->sortBy('ak_order', 'asc');
+// Load a list of filters; simply load json from block name
+$filters = json_decode(
+    ((string) $this->getBlockObject()->getBlockName()) ?: '{"groups": ["Staff"]}',
+    true
+);
 
 /**
- * Build doc with all the 'member's in it, and load the values we'll
- * need to display.
+ * Get a list of members to display
+ *
+ * @param array $filters Assoc array of the filters to apply
+ * @return array
  */
-$members = [];
-foreach ($ul->get(100) as $staffMember) {
-    $member = [];
-    $avatar = $av->getImagePath($staffMember);
-    if ($avatar) {
-        $member['background'] = $avatar;
-    } else {
-        $member['placeholder'] = 'placeholder' . (ord($staffMember->getUserID()) % 3);
+$getMembers = function($filters) use ($av) {
+    // Filter to show only the staff members, or customize
+    // by 'custom template' and choose a block name
+    $ul = new UserList();
+
+    $defaultAttribute = ['', true, '='];
+
+    foreach ($filters as $k => $filter) {
+        if ($k === 'groups') {
+            foreach ($filter as $group) {
+                $ul->filterByGroup($group);
+            }
+        } elseif ($k === 'attributes') {
+            // $attribute keys: 'id', 'value', 'compare'
+            foreach ($filter as $attribute) {
+                list($handle, $value, $comparison) = array_replace(
+                    $defaultAttribute,
+                    $attribute
+                );
+
+                $ul->filterByAttribute($handle, $value, $comparison);
+            }
+        }
+        $ul->sortBy('ak_order', 'asc');
     }
-    $member['shortbio'] = $staffMember->getAttribute('first_name') . ' ' .
-        $staffMember->getAttribute('last_name') . ', ' .
-        ($staffMember->getAttribute('job_title') ?: 'Jane\'s Walk');
-    
-    $member['email'] = $staffMember->getUserEmail();
-    $member['bio'] = $staffMember->getAttribute('bio');
-    $members[] = $member;
-}
 
-echo $controller->getContent();
-
+    /**
+     * Build doc with all the 'member's in it, and load the values we'll
+     * need to display.
+     */
+    return array_map(
+        function($member) use ($av) {
+            return [
+                'id' => $member->getUserID(),
+                'img' => $av->getImagePath($member),
+                'name' => trim($member->getAttribute('first_name') . ' ' . $member->getAttribute('last_name')),
+                'title' => $member->getAttribute('job_title') ?: "Jane's Walk",
+                'email' => $member->getUserEmail(),
+                'description' => $member->getAttribute('bio')
+            ];
+        },
+        $ul->get(100)
+    );
+};
 ?>
+<?= $controller->getContent() ?>
 <ul class="ccm-staff-list">
-<?php foreach ($members as $member) { ?>
+<?php foreach ($getMembers($filters) as $member) { ?>
     <li>
-        <div 
-            class="u-avatar <?= $member['placeholder'] ?>"
-            <?= $member['background'] ? (' style="background-image:url(' . $member['background'] . ')"') : '' ?>>
+        <div
+            class="u-avatar <?= 'placeholder' . (ord($member['id']) % 3) ?>"
+            <?= $member['img'] ? (' style="background-image:url(' . $member['img'] . ')"') : '' ?>>
         </div>
         <div class="ccm-staff-list-details">
             <h3>
-                <?= $member['shortbio'] ?>
+                <?= $member['name'] . ', ' . $member['title'] ?>
             </h3>
             <a href="mailto:<?= $member['email'] ?>">
-                <?= $member['email'] ?>        
+                <?= $member['email'] ?>
             </a>
             <p class="ccm-staff-list-bio">
-                <?= $member['bio'] ?>
+                <?= $member['description'] ?>
             </p>
         </div>
     </li>
