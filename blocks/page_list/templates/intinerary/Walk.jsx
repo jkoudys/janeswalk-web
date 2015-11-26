fcfa2b71f09eedd9cb47aebe2a7d5a4b14faabd0
Richard Bansal <richard.bansal@gmail.com>
2015-11-25 22:34:06 -0500

    1. updating onClick

--- blocks/page_list/templates/intinerary/Walk.jsx
+++ blocks/page_list/templates/intinerary/Walk.jsx
@@ -3,14 +3,13 @@ import {dateFormatted} from './Itinerary-Utils'
 export default ({walk,remove,key}) => {
 	return (
 			<div>
-
 					<div id="walkDetails">
 							<h4>{walk.title}</h4>
 							<h4>{dateFormatted(walk.time.slots[0][0])}</h4>
 					</div>
 
 					<div id="removeWalk">
-							<button onClick{remove.bind(null,walk.id)}> remove </button>
+							<button onClick={remove.bind(null,walk.id)}> remove </button>
 					</div>
 
 			</div>
