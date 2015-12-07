08a67c58cd877b72635c319798c85228fc6194df
Richard Bansal <richard.bansal@gmail.com>
2015-12-07 12:59:06 -0500

    1. fixing spacing for PR

--- blocks/page_list/templates/itinerary/AddWalkToListDialog.jsx
+++ blocks/page_list/templates/itinerary/AddWalkToListDialog.jsx
@@ -4,49 +4,50 @@ export default class AddWalkToListDialog extends React.component {
 		}
 
 		propTypes() {
-				return {
-						lists: React.PropTypes.array,
-						add: React.PropTypes.func,
-						createList: React.PropTypes.func,
-						walkSelected: React.PropTypes.number,
-				}
+    return {
+      lists: React.PropTypes.array,
+      add: React.PropTypes.func,
+      createList: React.PropTypes.func,
+      walkSelected: React.PropTypes.number,
+    }
 		}
 
 		render() {
-				const {lists, add, createList, activeWalk} = this.props;
-				//selectedWalk comes from where
+    const {lists, add, createList, activeWalk} = this.props;
+    //selectedWalk comes from where
 
-				const allLists = lists.map(list => {
+    const allLists = lists.map(list => {
 
-						const {id,title,walks} = list;
+      const {id,title,walks} = list;
 
-						const walkFound = walks.find(walk => walk.id === activeWalk);
+      const walkFound = walks.find(walk => walk.id === activeWalk);
 
-						if(walkFound){
-								return ( //FIX: Issue with onClick, only available for last item in the list
-												<li onClick={(ev) => add(activeWalk,list,ev.target.value)} key={id}>
-														<span className="glyphicon glyphicon-ok">{title}</span>
-												</li>
-								)
-						} else {
-								return (
-												<li key={id}>
-														<span>{title}</span>
-												</li>
-								)
-						}
-				});
+      if(walkFound){
+        return ( //FIX: Issue with onClick, only available for last item in the list
+          <li onClick={(ev) => add(activeWalk,list,ev.target.value)} key={id}>
+            <span className="glyphicon glyphicon-ok">{title}</span>
+          </li>
+        )
+      } else {
+        return (
+          <li key={id}>
+            <span>{title}</span>
+          </li>
+        )
+      }
+    });
 
-				return(
-								<dialog id="addWalk">
-										<ul>
-												{allLists}
-										</ul>
-										<input ref="list" placeholder="Create a new List"></input>
-										<button onClick={(ev) => createList(walkSelected.id, this.refs.list.value)}>Create</button>
-										<button onClick={(ev) => document.getElementById('addWalk').close()}>Close</button>
-								</dialog>
-				)
-		}
+    return(
+      <dialog id="addWalk">
+        <ul>
+          {allLists}
+        </ul>
 
+        <input ref="list" placeholder="Create a new List"></input>
+
+        <button onClick={(ev) => createList(walkSelected.id, this.refs.list.value)}>Create</button>
+        <button onClick={(ev) => document.getElementById('addWalk').close()}>Close</button>
+      </dialog>
+    )
+  }
 }
