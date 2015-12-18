7b44e73c5194713541e3bc3c519c89f90b1d9ca5
Richard Bansal <richard.bansal@gmail.com>
2015-12-18 12:30:19 -0500

    1. updating Accessibility with correct text 2. fixing spacing

--- themes/janeswalk/js/components/pages/WalkPage/WalkAccessibility.jsx
+++ themes/janeswalk/js/components/pages/WalkPage/WalkAccessibility.jsx
@@ -4,17 +4,21 @@ const WalkAccessibility = ({checkboxes}) => {
 
   const accessibilityKeys = Object.keys(checkboxes).filter(item => item.includes("accessible"));
 
-  //TODO: Need to format accessibility further so accessible-familyfriendly should be Family Friendly (create a function to do this)
+  const accessibility = {
+    "accessible-familyfriendly":"Family friendly",
+    "accessible-seniors":"Senior Friendly",
+    "accessible-busy":"Busy sidewalks",
+  };
 
- return (
-   <section id="walkAccessibility">
-     <a name="Accessibility"></a>
-     <h2>Accessibility</h2>
-     <ul>
-       {accessibilityKeys.map(k => (<li><input type="checkbox" disabled="disabled" checked={checkboxes[k] ? "checked" : ""} value={k}></input></li>))}
-     </ul>
-   </section>
- );
+  return (
+    <section id="walkAccessibility">
+      <a name="Accessibility"></a>
+      <h2>Accessibility</h2>
+      <ul>
+        {accessibilityKeys.map(k => {return <li><input type="checkbox" disabled="disabled" checked={checkboxes[k] ? "checked" : ""}></input>{accessibility[k]}</li>})}
+      </ul>
+    </section>
+  );
 };
 
 WalkAccessibility.propTypes = {
