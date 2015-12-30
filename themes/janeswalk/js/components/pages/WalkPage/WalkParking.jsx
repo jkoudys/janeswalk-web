a4a534796e981c4f9caa96250887fb34e6e86c98
Richard Bansal <richard.bansal@gmail.com>
2015-12-30 14:49:16 -0500

    1. adding WalkParking component

---
+++ themes/janeswalk/js/components/pages/WalkPage/WalkParking.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WalkParking = ({accessibleParking, style}) => (
+    <section className={`walkParking ${style}`}>
+     {style === 'walk-page' ? <a name="Parking Availability"></a> : ''}
+     <h2>Parking Availability</h2>
+     {accessibleParking}
+    </section>
+);
+
+WalkParking.propTypes = {
+ accessibleParking: React.PropTypes.string.isRequired,
+};
+
+export default WalkParking;
