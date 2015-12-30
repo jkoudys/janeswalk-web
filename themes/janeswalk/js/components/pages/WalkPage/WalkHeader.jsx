e23c2851f6c009b42f879f37e28db0b0f9365b54
Richard Bansal <richard.bansal@gmail.com>
2015-12-30 14:42:03 -0500

    1. Adding breadcrumbs using stubbed data

--- themes/janeswalk/js/components/pages/WalkPage/WalkHeader.jsx
+++ themes/janeswalk/js/components/pages/WalkPage/WalkHeader.jsx
@@ -4,26 +4,35 @@ import React from 'react';
 import { dateFormatted } from '../../../../../../blocks/page_list/templates/itinerary/itinerary/ItineraryUtils';
 
 //TODO: Duplicate of Itinerary <Walk/>
+//TODO: Issue with Favourite being removed on first attempt (works fine for Itinerary)
 
-const WalkHeader = ({walk, id, remove, add, existsInItinerary, existsInFavourites, favoriteListId, itineraryListId}) => {
-
+const WalkHeader = ({city, walk, id, remove, add, existsInItinerary, existsInFavourites, favoriteListId, itineraryListId}) => {
+  debugger;
   const favButton = () => {
     if (existsInFavourites) return <button className="removeFavourite" onClick={()=>remove(id,favoriteListId)}> </button>;
-    return <button className="addFavourite" onClick={()=>add(id,favoriteListId)}> </button>;
+    else return <button className="addFavourite" onClick={()=>add(id,favoriteListId)}> </button>;
   };
 
   const addButton = () => {
-    if (existsInItinerary) return <button className="removeItinerary" onClick={()=>remove(id,itineraryListId)}>I'm going</button>;
-    return <button className="addItinerary" onClick={()=>add(id,itineraryListId)}> </button>;
+    if (existsInItinerary) return <button className="removeItinerary" onClick={()=>remove(id,itineraryListId)}></button>;
+    else return <button className="addItinerary" onClick={()=>add(id,itineraryListId)}></button>;
   };
 
   const addToFavourites = favButton();
   const addToItinerary = addButton();
   const {title, map, time, team} = walk;
+  const {url, name} = city;
   const walkLeader = team.find(member => member.role === 'walk-leader');
 
   return(
     <section className="walkHeader">
+    <section className="coverImage">
+      <ul className="breadcrumb">
+        <li><a href="/"><i className="fa fa-home"></i></a></li>
+        <li><a href={url}>{`${name} walks}</a></li>
+        <li className="active">{title}</li>
+      </ul>
+    </section>
       <h1>{title}{addToFavourites}</h1>
       <h4>Led By {walkLeader['name-first']} {walkLeader['name-last']} - {dateFormatted(time.slots[0][0])}{addToItinerary}</h4>
       <h4>Meeting at {map.markers[0].title}</h4>
