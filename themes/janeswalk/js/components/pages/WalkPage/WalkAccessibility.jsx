31711deb55ca8805a378f76ecf48c1ae5162d20e
Richard Bansal <richard.bansal@gmail.com>
2015-12-30 14:36:44 -0500

    1. updating WalkAccessibility to remove static filters

--- themes/janeswalk/js/components/pages/WalkPage/WalkAccessibility.jsx
+++ themes/janeswalk/js/components/pages/WalkPage/WalkAccessibility.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 
-const WalkAccessibility = ({checkboxes}) => {
+const WalkAccessibility = ({checkboxes, accessibility, style}) => {
 
   const accessibilityKeys = Object.keys(checkboxes).filter(item => item.includes("accessible"));
 
-  const accessibility = {
-    "accessible-familyfriendly":"Family friendly",
-    "accessible-seniors":"Senior Friendly",
-    "accessible-busy":"Busy sidewalks",
-  };
-
   return (
-    <section className="walkAccessibility">
-      <a name="Accessibility"></a>
+    <section className={`walkAccessibility ${style}`}>
+      {style === 'walk-page' ? <a name="Accessibility"></a> : ''}
       <h2>Accessibility</h2>
       <ul>
-        {accessibilityKeys.map((k,i) => (<li key={i}><input type="checkbox" disabled="disabled" checked={checkboxes[k] ? "checked" : ""}></input>{accessibility[k]}</li>))}
+        {accessibilityKeys.map((k,i) => (<li key={i}>{accessibility.data[k.split('-')[1]]}</li>))}
       </ul>
     </section>
   );
