874c0b3cdb42b2e3ad19e66f69a60b27537d96d1
Richard Bansal <richard.bansal@gmail.com>
2016-01-05 11:35:36 -0500

    1. Summary now includes t2 pluralization

--- themes/janeswalk/js/components/pages/Dashboard/DashboardSummary.jsx
+++ themes/janeswalk/js/components/pages/Dashboard/DashboardSummary.jsx
@@ -1,19 +1,12 @@
 
-//TODO: Need to find or solidify the data source for impact data - some items not covered (in dashboard or impact static data) for below recap (Post PR)
-//TODO: Bring in <SingleBarGraph/> (Post PR)
-//TODO: Place all data collections items within the Store, remove reduces from below - also summary is not working, so remove this from here (Post PR)
-//TODO: total walk leaders: ${impact.walkLeaders.reduce((p,c)=>(p.total + c.total),0)} (Post PR)
-//TODO: total walks: ${impact.walks.reduce((p,c)=>(p.total + c.total),0)} (Post PR)
-//TODO: Place all .length calls in Store instead. (Post PR)
-
-const DashboardSummary = ({name, walkLeaders, walks, users, impact}) => {
-  debugger;
+//t2('Walk Leader: ', 'Walk Leaders: ', count($w->walkLeaders));
+const DashboardSummary = ({year, walkLeaders, walks, participants, originalYear, totalWalkLeaders, totalWalks, name }) => {
   return (
     <section>
       <h2>Recap</h2>
-      <h4>{`${walkLeaders.length} ${name} walk leaders led ${walks.length} walks as a part of Jane's Walk ${(new Date()).getFullYear()} reaching more than ${users.length} participants` }</h4>
+      <h4>{`${t2({name} + 'walk leader', {name} + 'walk leaders', walkLeaders)} led ${t2('walk', 'walks', walks)} as a part of Jane's Walk ${year} reaching more than ${t2('participant', 'participants', participants)}` }</h4>
 
-      <h4>{`Since ${name} first participated in Jane's Walk in ${impact.walkLeaders[0].year}, 87 walk leaders have led 121 Jane's Walks`}</h4>
+      <h4>{`Since ${name} first participated in Jane's Walk in ${originalYear}, ${t2('walk leader', 'walk leaders', totalWalkLeaders)} have led ${t2(totalWalks + 'Jane\'s Walk', totalWalks + 'Jane\'s Walks', totalWalks)}`}</h4>
     </section>
   );
 };
