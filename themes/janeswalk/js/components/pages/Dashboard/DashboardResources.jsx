1924ab27e503d9d48563327418ecdf866486be5f
Richard Bansal <richard.bansal@gmail.com>
2016-01-05 11:25:58 -0500

    1. updating DashboardResources

--- themes/janeswalk/js/components/pages/Dashboard/DashboardResources.jsx
+++ themes/janeswalk/js/components/pages/Dashboard/DashboardResources.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import DashboardStore from './DashboardStore';
 
-//TODO: Add styling, reference styling below, also City and Country Data is not available (assumed Toronto, Canada)
+const grabProps = ({route}) => {
+  return {cityOrganizers, videoTips, files, featuredWalks} = route
+};
 
-const DashboardResources = () => {
-  const {cityOrganizers, videoTips, files, featuredWalks} = DashboardStore.getResources();
+const DashboardResources = ({route}) => {
+  const {cityOrganizers, videoTips, files, featuredWalks} = route;
   return (
-
     <section>
       <h2>Resources</h2>
 
@@ -14,7 +15,7 @@ const DashboardResources = () => {
         <h3>Connect with Fellow organizers</h3>
         Got a question? Reach out to a fellow City Organizer for help.
         <ul>
-          {cityOrganizers.map(co => (<li>{`${co.firstName} from ${co.cityName}: ${co.email}`}</li>))}
+          {cityOrganizers.map((co,i) => (<li key={i}>{`${co.firstName} from ${co.cityName}: ${co.email}`}</li>))}
         </ul>
       </section>
 
@@ -22,9 +23,9 @@ const DashboardResources = () => {
         <h3>Walks from around the world</h3>
         Don't know what kind of walk to lead? Here are some fun ones from around the world
         <ul>
-          {featuredWalks.map(w =>
+          {featuredWalks.map((w,i) =>
             (
-              <li>
+              <li key={i}>
                 <h4>Toronto, ON</h4>
                 <a href={w.url}>{`${w.title}`}</a><br/>
                 <img src={`http://janeswalk.org/${w.thumbnailUrl}`}/>
@@ -39,8 +40,8 @@ const DashboardResources = () => {
         <h3>Tips on leading a walk</h3>
         Leading your first or fifth walk? Here are some tips from the Jane's Walk crew?
         <ul>
-          {videoTips.map(v => (
-            <li>
+          {videoTips.map((v,i) => (
+            <li key={i}>
               <iframe src={`${v}`} width="250" height="140" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </li>)
           )}
@@ -52,50 +53,11 @@ const DashboardResources = () => {
         Want help promoting Jane's Walk?
         Use these files to promote Jane's Walk in your city
         <ul>
-          {files.map(f => (<li><a href={f.url}>{`${f.name}`}</a></li>))}
+          {files.map((f,i) => (<li key={i}><a href={f.url}>{`${f.name}`}</a></li>))}
         </ul>
       </section>
     </section>
   );
-};
+}
 
 export default DashboardResources;
-
-//TODO: Match styles from previous walk dashboard and updated styles (list vs. view): https://github.com/jkoudys/janeswalk-web/commit/892005006ee791fff30a13a774c2653d31612831
-//<div class="ideas resourceBlock">
-//  -                <div class="headline"><?= t('Walks from around the world') ?></div>
-//  -                <p>
-//  -                    <?= t('Don\'t know what kind of walk to lead?') ?><br />
-//  -                    <?= t('Here are some fun ones from around the world') ?>
-//  -                </p>
-//  -                <ul>
-//  -                    <?php foreach ((array) $featuredWalkData as $featuredWalk) { ?>
-//  -                    <li>
-//  -                        <div class="banner" style="background-image: url('<?= ($featuredWalk['walkImagePath']) ?>');"></div>
-//  -                        <div class="meta">
-//  -                            <img src="/themes/janeswalk/images/countryFlags/<?= ($featuredWalk['countryName']) ?>.png" class="flag" />
-//  -                            <div class="city"><?= ($featuredWalk['cityName']) ?></div>
-//  -                            <div class="title">
-//  -                                <a href="<?= ($featuredWalk['walkPath']) ?>"><?= ($featuredWalk['walkTitle']) ?></a>
-//  -                            </div>
-//  -                        </div>
-//  -                    </li>
-//  -                    <?php } ?>
-//  -                </ul>
-//  -            </div>
-
-//<div class="vimeos resourceBlock">
-//  -                <div class="headline"><?= t('Tips on leading a walk') ?></div>
-//  -                <p>
-//  -                    <?= t('Leading your first or fith walk?') ?><br />
-//  -                    <?= t('Here are some tips from the Jane\'s Walk crew') ?>
-//  -                </p>
-//  -                <ul>
-//  -                    <?php foreach ($resourceVideos as $video) { ?>
-//  -                    <li>
-//  -                        <iframe src="<?= $video ?>" width="250" height="140" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-//  -                    </li>
-//  -                    <?php } ?>
-//  -                </ul>
-//  -            </div>
-//=======
