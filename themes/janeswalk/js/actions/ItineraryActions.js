b2601a2d2537e59ebdf6f0ba9c3703a40c135f52
Richard Bansal <richard.bansal@gmail.com>
2016-01-21 16:46:51 -0500

    1. reverting city page 2. updating Itinerary Action constants

--- themes/janeswalk/js/actions/ItineraryActions.js
+++ themes/janeswalk/js/actions/ItineraryActions.js
@@ -6,35 +6,35 @@ import {dispatch} from 'janeswalk/dispatcher/AppDispatcher';
 export default {
 
   remove(id, list) {
-    dispatch({type: ActionTypes.REMOVE_WALK, id, list});
+    dispatch({type: ActionTypes.ITINERARY_REMOVE_WALK, id, list});
   },
 
   add(id, list) {
-    dispatch({type: ActionTypes.ADD_WALK, id, list});
+    dispatch({type: ActionTypes.ITINERARY_ADD_WALK, id, list});
   },
 
   updateTitle(title) {
-    dispatch({type: ActionTypes.UPDATE_TITLE, title});
+    dispatch({type: ActionTypes.ITINERARY_UPDATE_TITLE, title});
   },
 
   updateDescription(description) {
-    dispatch({type: ActionTypes.UPDATE_DESCRIPTION, description});
+    dispatch({type: ActionTypes.ITINERARY_UPDATE_DESCRIPTION, description});
   },
 
   viewList(id) {
-    dispatch({type: ActionTypes.VIEW_LIST, id});
+    dispatch({type: ActionTypes.ITINERARY_VIEW_LIST, id});
   },
 
   createList(id, title) {
-    dispatch({type: ActionTypes.CREATE_LIST, id, title});
+    dispatch({type: ActionTypes.ITINERARY_CREATE_LIST, id, title});
   },
 
   walkSelected(id) {
-    dispatch({type: ActionTypes.WALK_SELECTED, id});
+    dispatch({type: ActionTypes.ITINERARY_WALK_SELECTED, id});
   },
 
   addWalkDialog() {
-    dispatch({type: ActionTypes.ADD_WALK_DIALOG});
+    dispatch({type: ActionTypes.ITINERARY_ADD_WALK_DIALOG});
   }
 
 }
