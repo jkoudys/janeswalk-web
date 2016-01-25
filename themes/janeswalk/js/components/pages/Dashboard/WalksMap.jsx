7d5f7401c6c2ef47a91ac17bf5b411df3259db67
Richard Bansal <richard.bansal@gmail.com>
2016-01-25 17:25:23 -0500

    1. Updating Dashboard <WalksMap/> styling

--- themes/janeswalk/js/components/pages/Dashboard/WalksMap.jsx
+++ themes/janeswalk/js/components/pages/Dashboard/WalksMap.jsx
@@ -2,9 +2,9 @@ import DashboardStore from './DashboardStore';
 
 //TODO: (Post-PR) WalkMap.jsx already exists, review and re-use, you have a few usages of the google map that can be combined
 
-const InfoWindow = ({title, shortDescription}) => (
+const InfoWindow = ({url, title, shortDescription}) => (
   <span>
-    <h4>{title}</h4>
+    <h4><a href={url}>{title}</a></h4>
     <p>{shortDescription}</p>
   </span>
 );
@@ -40,6 +40,7 @@ const manageMarkers = (map, markers, walks) => {
       if (!walkFound) {
         let marker = new google.maps.Marker({
           position: locationLatLng,
+          style: 'stop',
           map: map,
           walkId: walk.id,
         });
@@ -90,11 +91,82 @@ export default class WalksMap extends React.Component {
 
     const mapOptions = {
       center: locationLatLng,
-      zoom: 11,
+      zoom: 12,
+      scrollwheel: false,
       backgroundColor: '#d7f0fa',
     };
 
     const googleMap = new google.maps.Map(ReactDOM.findDOMNode(this), mapOptions);
+
+    googleMap.mapTypes.set('map_style', new google.maps.StyledMapType([{
+      featureType: "poi.park",
+      elementType: "geometry.fill",
+      stylers: [{
+        visibility: "on"
+      }, {
+        saturation: 37
+      }]
+    }, {
+      featureType: 'landscape',
+      stylers: [{
+        visibility: 'on'
+      }, {
+        color: '#eaeaea'
+      }]
+    }, {
+      featureType: 'poi',
+      stylers: [{
+        visibility: 'off'
+      }]
+    }, {
+      featureType: 'poi.park',
+      stylers: [{
+        visibility: 'on'
+      }, {
+        color: '#cadfaa'
+      }]
+    }, {
+      featureType: 'poi.school',
+      elementType: 'labels',
+      stylers: [{
+        visibility: 'off'
+      }]
+    }, {
+      featureType: 'poi.school',
+      elementType: 'geometry',
+      stylers: [{
+        visibility: 'on'
+      }, {
+        color: '#dadada'
+      }]
+    }, {
+      featureType: 'transit',
+      stylers: [{
+        visibility: 'off'
+      }]
+    }, {
+      featureType: 'water',
+      stylers: [{
+        visibility: 'simplified'
+      }, {
+        color: '#90c2ff'
+      }]
+    }, {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{
+        visibility: 'simplified'
+      }, {
+        color: '#ffffff'
+      }]
+    }, {
+      featureType: 'road',
+      elementType: 'labels.icon',
+      stylers: [{
+        visibility: 'off'
+      }]
+    }]));
+    googleMap.setMapTypeId('map_style');
 
     this.setState({googleMap, googleMapMarkers: manageMarkers(googleMap, googleMapMarkers, walks)});
   }
