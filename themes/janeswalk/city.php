c4e626373061a5bfdfb6226f4e936fe0445cafb5
Richard Bansal <richard.bansal@gmail.com>
2016-01-28 12:20:08 -0500

    1. Initial city.php for styling

--- themes/janeswalk/city.php
+++ themes/janeswalk/city.php
@@ -133,25 +133,21 @@ echo <<< EOT
      {$area('City Header')}
      {$BackgroundPhoto}
      {$CityOrganizerDetails}
+     <div class="menu-flags">
+              {$area('City Nav')}
+      </div>
  </div>
 </section>
 <section id="city-details">
- <div class="description">
-     <div class="item">
-         <h2>{$t('Jane’s Walks')}</h2>
-         <h4>{$t('Get out and walk! Explore, learn and share through a Jane’s Walk in %s', $cityName)}</h4>
-         {$longDescription}
-         {$area('City Description')}
-     </div>
-     <div class="menu-flags">
-         {$area('City Nav')}
-     </div>
-     {$area('Sponsors')}
- </div>
  <div class="walk-list">
+      <div class="item">
+          <h3>{$t('Walks in %s', $cityName)}</h3>
+      </div>
      <a href="{$this->url('/walk/form')}?parentCID={$c->getCollectionID()}" class="create-walk"><i class="fa fa-star"></i> {$t('Create a Walk')}</a>
-     <h3>{$t('Walks in %s', $cityName)}</h3>
      {$area('All Walks List')}
+ </div>
+ <div class="description">
+     {$area('Sponsors')}
  </div>
 </section>
 {$Blog}
