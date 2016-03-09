dc84c21c80e92cb0c438aabc02543df3c6b7915b
jkoudys <jkoudys@gmail.com>
2016-03-09 11:17:42 -0500

    More refactor cleanup

--- modules/janeswalk/stores/I18nStore.js
+++ modules/janeswalk/stores/I18nStore.js
@@ -6,53 +6,26 @@
 
 // Basic flux setup
 import {EventEmitter} from 'events';
-import {register} from 'janeswalk/dispatcher/AppDispatcher';
-import {ActionTypes} from 'janeswalk/constants/JWConstants';
+import {register2} from 'janeswalk/dispatcher/AppDispatcher';
+import {ActionTypes as AT} from 'janeswalk/constants/JWConstants';
+import {changeMethods} from 'janeswalk/utils/Stores';
 
 // The library for managing translations
 import I18nTranslator from '../utils/translate.js';
 
-// Simple 'something has changed' event
-const CHANGE_EVENT = 'change';
-
 // Local vars
 const _i18n = new I18nTranslator();
 
-const I18nStore = Object.assign({}, EventEmitter.prototype, {
-  emitChange() {
-    this.emit(CHANGE_EVENT);
-  },
+const I18nStore = Object.assign({}, EventEmitter.prototype, changeMethods, {
+  getTranslate: () => _i18n.translate.bind(_i18n),
+  getTranslatePlural: () => _i18n.translatePlural.bind(_i18n),
 
-  /**
-   * @param {function} callback
-   */
-  addChangeListener(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
-
-  getTranslate() {
-    return _i18n.translate.bind(_i18n);
-  },
-
-  getTranslatePlural() {
-    return _i18n.translatePlural.bind(_i18n);
-  }
-});
-
-// Register our dispatch token as a static method
-I18nStore.dispatchToken = register(payload => {
-  // Go through the various actions
-  switch(payload.type) {
-    // POI actions
-    case ActionTypes.I18N_RECEIVE:
-      _i18n.constructor(payload.translations);
-      I18nStore.emitChange();
-    break;
-    default:
-      // do nothing
-  }
+  // Register our dispatch token as a static method
+  dispatchToken: register2({
+    [AT.I18N_RECEIVE]: ({translations}) => _i18n.constructor(translations)
+  }, () => I18nStore.emitChange())
 });
 
 export default I18nStore;
-export let t = _i18n.translate.bind(_i18n);
-export let t2 = _i18n.translatePlural.bind(_i18n);
+export let t = I18nStore.getTranslate();
+export let t2 = I18nStore.getTranslatePlural();
