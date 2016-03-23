9122ab4f7b4acd82a756e77a7a79f0ae1cedc6a5
jkoudys <jkoudys@gmail.com>
2016-03-23 16:45:23 -0400

    Build links for mailtos

--- themes/janeswalk/js/janeswalk.js
+++ themes/janeswalk/js/janeswalk.js
@@ -10667,9 +10667,18 @@
 
 	  var formatted = children.split(/ /).map(function (word) {
 	    if (word.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/)) {
+	      var prefix = '';
+	      if (!word.includes('http')) {
+	        if (word.includes('@')) {
+	          prefix = 'mailto:';
+	        } else {
+	          prefix = 'http://';
+	        }
+	      }
+
 	      return _react2.default.createElement(
 	        'a',
-	        { href: word.includes('http') ? word : 'http://' + word, target: '_blank' },
+	        { href: '' + prefix + word, target: '_blank' },
 	        word,
 	        ' '
 	      );
