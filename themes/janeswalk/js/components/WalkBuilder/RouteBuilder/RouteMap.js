a540bb6b8dfb2f5ceec034bf6894f340b3cfc6b6
Joshua Koudys <josh@qaribou.com>
2016-12-20 07:31:16 -0500

    Add route map

--- themes/janeswalk/js/components/WalkBuilder/RouteBuilder/RouteMap.js
+++ themes/janeswalk/js/components/WalkBuilder/RouteBuilder/RouteMap.js
@@ -3,49 +3,78 @@
  *
  * Show and draw your route.
  */
-/* global google */
+/* global CCM_THEME_PATH */
 
 import { createElement as ce, Component } from 'react';
+import GoogleMap from 'google-map-react';
+
+const StopMarker = () => ce('img', { src: `${CCM_THEME_PATH}/images/marker.png` });
+
+const mapStyle = [{
+  featureType: 'transit',
+  stylers: [{
+    visibility: 'off',
+  }],
+}, {
+  featureType: 'water',
+  stylers: [{
+    visibility: 'simplified',
+  }, {
+    color: '#d7f0fa',
+  }],
+}, {
+  featureType: 'road',
+  elementType: 'geometry',
+  stylers: [{
+    visibility: 'simplified',
+  }, {
+    color: '#ffffff',
+  }],
+}];
 
 export default class RouteMap extends Component {
-  componentDidMount() {
-    // Render the gmap
-    // Calling this 'mapDiv' to align with google maps api reference
-    const { mapDiv } = this;
-    const { city } = this.props;
+  constructor(props) {
+    super(props);
 
-    // The div renders after this step, so we need to defer the gmaps render
-    setTimeout(() => {
-      const gmap = new google.maps.Map(mapDiv, {
+    Object.assign(this, {
+      createMapOptions: (maps) => ({
         zoom: 12,
-        center: new google.maps.LatLng(city.latlng[0], city.latlng[1]),
+        center: new maps.LatLng(this.props.city.latlng[0], this.props.city.latlng[1]),
         scrollwheel: false,
         rotateControl: true,
         scaleControl: true,
         backgroundColor: '#d7f0fa',
         mapTypeControl: false,
         zoomControl: true,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeId: maps.MapTypeId.TERRAIN,
         mapTypeControlOptions: {
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE],
+          mapTypeIds: [maps.MapTypeId.ROADMAP, maps.MapTypeId.SATELLITE],
         },
-      });
-      this.setState({
-        gmap,
-      });
-    }, 0);
+      }),
+      afterMapLoaded: ({ map, maps }) => {
+        map.mapTypes.set('map_style', new maps.StyledMapType(mapStyle));
+        map.setMapTypeId('map_style');
+      },
+    });
   }
 
   render() {
+    const { createMapOption, afterMapLoaded } = this;
+
     return (
-      ce('div', {
-        ref: mapDiv => Object.assign(this, { mapDiv }),
-        className: 'Lead__RouteMap',
+      ce(GoogleMap, {
+        options: createMapOption,
+        onGoogleApiLoaded: afterMapLoaded,
+        defaultZoom: 12,
+        defaultCenter: this.props.city.latlng,
         style: {
           width: '100%',
           height: `${window.innerHeight * 0.6}px`,
+          position: 'relative',
         },
-      })
+      },
+        ce(StopMarker, { lat: this.props.city.latlng[0], lng: this.props.city.latlng[1] })
+      )
     );
   }
 }
