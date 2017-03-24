418ccb138e9f776e2b688d7bb4d789f2d6267e25
Joshua Koudys <josh@qaribou.com>
2017-03-24 01:56:45 -0400

    Antify login modal

--- themes/janeswalk/js/components/Login.jsx
+++ themes/janeswalk/js/components/Login.jsx
@@ -1,131 +1,159 @@
 /* global $ CCM_REL */
 
-import { t } from 'janeswalk/stores/I18nStore';
-import React from 'react';
+import { createElement as ce, Component } from 'react';
+import t from 'es2015-i18n-tag';
+import { Modal } from 'antd';
+
+const { event } = window.JanesWalk;
 
 const Message = ({ success, msg, error }) => (
-  <div className={`alert alert-${success ? 'info' : 'danger'}`}>
-    {msg}{error}
-  </div>
+  ce('div', { className: `alert alert-${success ? 'info' : 'danger'}` },
+    msg,
+    error,
+  )
 );
 
 /**
  * The 'login' modal that comes up on standard login, not to be confused
  * with the login page.
  */
-export default class Login extends React.Component {
-  constructor() {
-    super();
+export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    maintainLogin: false,
+    message: {},
+  };
 
-    Object.assign(this, {
-      state: {
-        email: '',
-        password: '',
-        maintainLogin: false,
-        message: {},
-      },
-      handleReset: () => {
-        const body = new FormData();
-        body.append('uEmail', this.state.email);
-        body.append('uName', this.state.email);
-        body.append('format', 'JSON');
+  handleReset = () => {
+    const body = new FormData();
+    body.append('uEmail', this.state.email);
+    body.append('uName', this.state.email);
+    body.append('format', 'JSON');
 
-        // Post a reset request to the c5 endpoint for resets
-        fetch(`${CCM_REL}/login/forgot_password`, {
-          method: 'POST',
-          credentials: 'include',
-          body,
-        })
-        .then(res => res.json())
-        .then(json => this.setState({ message: json }))
-        .catch(ex => console.error(`Error resetting password: ${ex.message}`));
-      },
+    // Post a reset request to the c5 endpoint for resets
+    fetch(`${CCM_REL}/login/forgot_password`, {
+      method: 'POST',
+      credentials: 'include',
+      body,
+    })
+    .then(res => res.json())
+    .then(json => this.setState({ message: json }))
+    .catch(ex => console.error(`Error resetting password: ${ex.message}`));
+  };
 
-      handleChangeEmail: (ev) => {
-        this.setState({ email: ev.target.value });
-      },
+  handleChangeEmail = (ev) => {
+    this.setState({ email: ev.target.value });
+  };
 
-      handleChangePassword: (ev) => {
-        this.setState({ password: ev.target.value });
-      },
+  handleChangePassword = (ev) => {
+    this.setState({ password: ev.target.value });
+  };
 
-      handleChangeMaintainLogin: (ev) => {
-        this.setState({ maintainLogin: ev.target.value });
-      },
+  handleChangeMaintainLogin = (ev) => {
+    this.setState({ maintainLogin: ev.target.value });
+  };
 
-      handleSubmit: (ev) => {
-        ev.preventDefault();
-        const body = new FormData();
-        body.append('uEmail', this.state.email);
-        body.append('uName', this.state.email);
-        body.append('uPassword', this.state.password);
-        body.append('uMaintainLogin', this.state.maintainLogin);
-        body.append('format', 'JSON');
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    const body = new FormData();
+    body.append('uEmail', this.state.email);
+    body.append('uName', this.state.email);
+    body.append('uPassword', this.state.password);
+    body.append('uMaintainLogin', this.state.maintainLogin);
+    body.append('format', 'JSON');
 
-        // Post the login to the c5 endpoint for logins
-        fetch(`${CCM_REL}/login/do_login`, {
-          method: 'POST',
-          credentials: 'include',
-          body,
-        })
-        .then(res => res.json())
-        .then(data => {
-            this.setState({ message: data }, () => {
-              if (data.success === 1) {
-                if (this.props.redirectURL) {
-                  window.location.replace(this.props.redirectURL);
-                } else {
-                  window.location.reload();
-                }
-              }
-            });
-        })
-        .catch(ex => console.error(`Error logging in: ${ex.message}`));
-      },
-    });
+    // Post the login to the c5 endpoint for logins
+    fetch(`${CCM_REL}/login/do_login`, {
+      method: 'POST',
+      credentials: 'include',
+      body,
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({ message: data }, () => {
+        if (data.success === 1) {
+          if (this.props.redirectURL) {
+            window.location.replace(this.props.redirectURL);
+          } else {
+            window.location.reload();
+          }
+        }
+      });
+    })
+    .catch(ex => console.error(`Error logging in: ${ex.message}`));
+  };
+
+  onCancel = () => this.setState({ visible: false });
+
+  componentWillMount() {
+    event.on('login', () => this.setState({ visible: true }));
   }
 
   render() {
     const { socialLogin } = this.props;
-    const { email, password } = this.state;
+    const { email, password, visible } = this.state;
+    const { onCancel } = this;
 
     let message;
     if (Number.isInteger(this.state.message.success)) {
-      message = <Message {...this.state.message} />;
+      message = ce(Message, this.state.message);
     }
 
     return (
-      <dialog id="login">
-        <div>
-          <article>
-            <header>
-              <h3 className="form-lead">{t('Sign in to %s', 'Jane\'s Walk')}</h3>
-            </header>
-            <form rel="form" method="post" onSubmit={this.handleSubmit}>
-              <section dangerouslySetInnerHTML={{ __html: socialLogin }} />
-              <section>
-                <h4>{t('or, log-in using your email & password')}</h4>
-                <label htmlFor="uEmail">
-                  {t('Email')}
-                  <input type="text" name="uEmail" id="uEmail" ref="uEmail" value={email} onChange={this.handleChangeEmail} className="ccm-input-text input-large" />
-                </label>
-                <label htmlFor="uPassword">{t('Password')}
-                  <input type="password" name="uPassword" id="uPassword" value={password} onChange={this.handleChangePassword} className="ccm-input-text input-large" />
-                </label>
-                <label>
-                  <input type="checkbox" name="uMaintainLogin" checked={this.maintainLogin} onChange={this.handleChangeMaintainLogin} /> {t('Keep me signed in.')}
-                </label>
-                <a onClick={this.handleReset}>{t('Request a new password')}</a>
-              </section>
-              <footer>
-                {message}
-                <a href={`${CCM_REL}/register?uEmail=${email}`}>{t('Register for a new account.')}</a>
-                <input type="submit" className="btn ccm-input-submit" id="submit" value={t('Go!')} />
-              </footer>
-            </form>
-          </article>
-        </div>
-      </dialog>
+      ce(Modal, {
+        id: 'login',
+        visible,
+        className: 'Login',
+        footer: null,
+        title: t`Sign in to ${'Jane\'s Walk'}`,
+        onCancel,
+        onOk: onCancel,
+      },
+        ce('form', { rel: 'form', method: 'post', onSubmit: this.handleSubmit },
+          ce('section', { dangerouslySetInnerHTML: { __html: socialLogin } }),
+          ce('section', {},
+            ce('h4', {}, t`'or, log-in using your email & password`),
+            ce('label', { htmlFor: 'uEmail' },
+              t`Email`,
+              ce('input', {
+                type: 'text',
+                name: 'uEmail',
+                id: 'uEmail',
+                ref: 'uEmail',
+                value: email,
+                onChange: this.handleChangeEmail,
+                className: 'ccm-input-text input-large',
+              }),
+            ),
+            ce('label', { htmlFor: 'uPassword' }, t`Password`,
+              ce('input', {
+                type: 'password',
+                name: 'uPassword',
+                id: 'uPassword',
+                value: password,
+                onChange: this.handleChangePassword,
+                className: 'ccm-input-text input-large',
+              }),
+            ),
+            ce('label', {},
+              ce('input', {
+                type: 'checkbox',
+                name: 'uMaintainLogin',
+                checked: this.maintainLogin,
+                onChange: this.handleChangeMaintainLogin,
+              }),
+              ' ', t`Keep me signed in.`,
+            ),
+            ce('a', { onClick: this.handleReset }, t`Request a new password`),
+          ),
+          ce('footer', {},
+            message,
+            ce('a', { href: `${CCM_REL}/register?uEmail=${email}` }, t`Register for a new account.`),
+            ce('input', { type: 'submit', className: 'btn ccm-input-submit', id: 'submit', value: t`Go!` }),
+          ),
+        )
+      )
     );
   }
 }
--- themes/janeswalk/js/components/Navbar/LoggedOutOptions.jsx
+++ themes/janeswalk/js/components/Navbar/LoggedOutOptions.jsx
@@ -2,11 +2,13 @@
 import { t, tc } from 'janeswalk/stores/I18nStore';
 import { createElement as ce } from 'react';
 
+const { event } = window.JanesWalk;
+
 export default ({ searching, toggleSearch }) => [
   ce('li', { key: 'nav2' },
     ce('a', { href: '/register' }, tc('Register on a website', 'Join')),
   ),
   ce('li', { key: 'nav3' },
-    ce('a', { onClick: () => $('#login').modal() }, t('Log in')),
+    ce('a', { onClick: () => event.emit('login') }, t('Log in')),
   ),
 ];
