3f57af9f4c4fb923e79a1082e9639a650a9cb3d1
Joshua Koudys <josh@qaribou.com>
2017-08-08 10:25:10 -0400

    Simplify profile by removing links into unused features

--- single_pages/profile/friends.php
+++ single_pages/profile/friends.php
@@ -1,46 +1,48 @@
-<?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 <div id="ccm-profile-wrapper">
-   <?php  Loader::element('profile/sidebar', array('profile'=> $profile)); ?>
-    <div id="ccm-profile-body">
-        <h1><?php echo t('My Friends') ?></h1>
-        <?php
-        $friendsData = UsersFriends::getUsersFriendsData( $profile->getUserID() );
-        if (!$friendsData) { ?>
+	<div id="ccm-profile-body">
+		<h1><?= t('My Friends') ?></h1>
+		<?php
+		$friendsData = UsersFriends::getUsersFriendsData( $profile->getUserID() );
+		if (!$friendsData) { ?>
 			<div style="padding:16px 0px;">
-				<?php echo t('No results found.')?>
+				<?=t('No results found.')?>
 			</div>
 		<?php
-        }else foreach ($friendsData as $friendsData) {
-            $friendUID=$friendsData['friendUID'];
-            $friendUI = UserInfo::getById( $friendUID );
-            if (!is_object($friendUI)) { ?>
-
-			<div class="ccm-users-friend" style="margin-bottom:16px;">
-				<div style="float:left; width:100px;">
-					<?php echo $av->outputNoAvatar()?>
-				</div>
-				<div >
-					<?php echo t('Unknown User')?>
-				</div>
-				<div class="ccm-spacer"></div>
-			</div>
-
-			<?php  } else { ?>
-			<div class="ccm-users-friend" style="margin-bottom:16px;">
-				<div style="float:left; width:100px;">
-					<a href="<?php echo View::url('/profile',$friendUID)?>"><?php echo  $av->outputUserAvatar($friendUI)?></a>
-				</div>
-				<div >
-					<a href="<?php echo View::url('/profile',$friendUID) ?>"><?php echo  $friendUI->getUsername(); ?></a>
-					<div style=" font-size:90%; line-height:90%; margin-top:4px;">
-					<?php echo t('Member Since') ?> <?php echo date(DATE_APP_GENERIC_MDY_FULL, strtotime($friendUI->getUserDateAdded('user')))?>
+		}
+		else {
+			$dh = Loader::helper('date');
+			/* @var $dh DateHelper */
+			foreach($friendsData as $friendsData) {
+				$friendUID = $friendsData['friendUID'];
+				$friendUI = UserInfo::getById( $friendUID );
+				if (!is_object($friendUI)) { ?>
+					<div class="ccm-users-friend" style="margin-bottom:16px;">
+						<div style="float:left; width:100px;">
+							<?= $av->outputNoAvatar() ?>
+						</div>
+						<div >
+							<?= t('Unknown User') ?>
+						</div>
+						<div class="ccm-spacer"></div>
 					</div>
-				</div>
-				<div class="ccm-spacer"></div>
-			</div>
-			<?php  } ?>
-		<?php  } ?>
-    </div>
-
+				<?php }
+				else { ?>
+					<div class="ccm-users-friend" style="margin-bottom:16px;">
+						<div style="float:left; width:100px; margin-right:10px;">
+							<a href="<?= View::url('/profile', $friendUID) ?>"><?= $av->outputUserAvatar($friendUI) ?></a>
+						</div>
+						<div >
+							<a href="<?= View::url('/profile', $friendUID) ?>"><?= $friendUI->getUsername(); ?></a>
+							<div style=" font-size:90%; line-height:90%; margin-top:4px;">
+							<?= t('Member Since %s', $dh->formatDate($friendUI->getUserDateAdded(), true)) ?>
+							</div>
+						</div>
+						<div class="ccm-spacer"></div>
+					</div>
+				<?php }
+			}
+		}
+?>
+	</div>
 	<div class="ccm-spacer"></div>
 </div>
